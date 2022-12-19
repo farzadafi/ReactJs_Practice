09d9b9e013845b72e1c7529e5d18fd703caf577b
farzadafi <farzadafi40@gmail.com>
2022-12-19 19:36:13 +0330

    create giveInformation function

---
+++ CW/Week1/CW01/q2/q2.js
@@ -0,0 +1,10 @@
+function giveInformation() {
+    let firstname = prompt("enter your first name:")
+    let lastname = prompt("enter your last name:")
+    let age = prompt("enter your age:")
+    let job = prompt("enter your job:")
+    console.log(`firstname is ${firstname}`)
+    console.log(`firstname is ${lastname}`)
+    console.log(`firstname is ${age}`)
+    console.log(`firstname is ${job}`)
+}
