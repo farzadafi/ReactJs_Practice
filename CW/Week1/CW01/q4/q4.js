10e9db7526f532e09d5eb83399014fd1d1b790be
farzadafi <farzadafi40@gmail.com>
2022-12-19 19:53:56 +0330

    add function for q4

---
+++ CW/Week1/CW01/q4/q4.js
@@ -0,0 +1,7 @@
+function giveNumber() {
+    let numberOne = prompt("enter first number:")
+    let numberTwo = prompt("enter second number:")
+
+    console.log(`first number is ${numberTwo}`)
+    console.log(`second number is ${numberOne}`)
+}
