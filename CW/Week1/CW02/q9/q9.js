85f7c31c89b866df5f507423db88a1d653ee50f0
farzadafi <farzadafi40@gmail.com>
2022-12-21 10:01:07 +0330

    add calcSum method

--- CW/Week1/CW02/q9/q9.js
+++ CW/Week1/CW02/q9/q9.js
@@ -1,3 +1,13 @@
 function getNumber() {
     let number = prompt("Enter your number:")
+    calcSum(number);
+}
+
+const calcSum = (num) => {
+    let array = num.split("");
+    let sum = 0;
+    for (const i in array) {
+        sum += Number(array[i])
+    }
+    alert(`sum for each element of your number is ${sum}`)
 }
