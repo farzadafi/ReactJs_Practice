1d9c2c70f24d87214383673ebcea0e073f0dfaa1
farzadafi <farzadafi40@gmail.com>
2022-12-21 09:15:44 +0330

    add getNumbers method

---
+++ CW/Week1/CW02/q7/q7.js
@@ -0,0 +1,6 @@
+function getNumbers() {
+    let numberOne = prompt("Enter number one:")
+    let numberTwo = prompt("Enter number two:")
+    let numberThree = prompt("Enter number three:")
+    const numbers = [numberOne, numberTwo, numberThree];
+}
