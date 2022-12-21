62d8e6f4d9165c1fabba98ef1815dbe0c990a12f
farzadafi <farzadafi40@gmail.com>
2022-12-21 09:32:44 +0330

    add calcMin function

--- CW/Week1/CW02/q7/q7.js
+++ CW/Week1/CW02/q7/q7.js
@@ -3,4 +3,14 @@ function getNumbers() {
     let numberTwo = prompt("Enter number two:")
     let numberThree = prompt("Enter number three:")
     const numbers = [numberOne, numberTwo, numberThree];
+    calcMin(numbers);
+}
+
+function calcMin(numbers) {
+    let min = numbers[0];
+    for (const i in numbers) {
+        if(numbers[i] < min)
+            min = numbers[i]
+    }
+    alert(`min number is ${min}`)
 }
