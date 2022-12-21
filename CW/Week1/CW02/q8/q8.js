e3e0abd43605cf2d32456aeebd56e6d9bf485e45
farzadafi <farzadafi40@gmail.com>
2022-12-21 09:44:58 +0330

    add reverse method

--- CW/Week1/CW02/q8/q8.js
+++ CW/Week1/CW02/q8/q8.js
@@ -1,4 +1,9 @@
 function getNumber() {
     let number = prompt("Enter your number:")
-
+    alert(`reverse is ${reverse(number)}`)
 }
+
+const reverse = (num) => parseInt(String(num)
+    .split("")
+    .reverse()
+    .join(""), 10);
