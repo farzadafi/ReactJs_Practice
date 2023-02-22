2c1f49c53b105fabdd65af9b74fb104aa910d08f
farzadafi <farzadafi40@gmail.com>
2023-02-22 15:58:37 +0330

    add new class to img tag

--- HW/Week10/divar_FirstPage/src/Components/FirstLogo/index.js
+++ HW/Week10/divar_FirstPage/src/Components/FirstLogo/index.js
@@ -3,7 +3,7 @@ import elementGenerator from "../../library/ElementGenerator.js";
 const logo = () => {
     return elementGenerator({
         element: 'img',
-        className: 'w-[6rem]',
+        className: 'w-[6rem] m-auto',
         src: 'src/assets/img.png',
         alt: 'divar logo png image'
     })
