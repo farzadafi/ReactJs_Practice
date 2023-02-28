4148f8c7b27482d5dc5be7e2e41d4316bafdf7ef
farzadafi <farzadafi40@gmail.com>
2023-02-28 17:21:43 +0330

    add new content

--- HW/Week11/person-information/tailwind.config.cjs
+++ HW/Week11/person-information/tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./main.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
