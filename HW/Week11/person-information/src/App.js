53591c07e88d18138b06aa5aa29abd6173f16bde
farzadafi <farzadafi40@gmail.com>
2023-02-28 17:33:29 +0330

    add App function

---
+++ HW/Week11/person-information/src/App.js
@@ -0,0 +1,12 @@
+import Container from "./layout/Container";
+import elementGenerator from "./library/elementGenerator";
+
+const App = () => {
+    const app = elementGenerator({
+        element:'div',
+        child:[]
+    })
+    return Container(app)
+}
+
+export default App;
