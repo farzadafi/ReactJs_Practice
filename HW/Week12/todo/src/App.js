774d51d29a21be3e4cc5262642dfe6e5d531aacf
farzadafi <farzadafi40@gmail.com>
2023-03-05 20:13:48 +0330

    fix import address

--- HW/Week12/todo/src/App.js
+++ HW/Week12/todo/src/App.js
@@ -1,5 +1,5 @@
-import Container from './layout/Container'
-import elementGenerator from './library/elementGenerator'
+import Container from '@/layout/Container'
+import elementGenerator from '@/library/elementGenerator'
 
 const App = () => {
   const app = elementGenerator({
