4cde2ba87af9c2ef6e0f89298935f0946a0ee22a
farzadafi <farzadafi40@gmail.com>
2023-03-05 19:50:56 +0330

    create Container function

---
+++ HW/Week12/todo/src/layout/Container/index.js
@@ -0,0 +1,12 @@
+import elementGenerator from '../../library/elementGenerator'
+
+const Container = (child) => {
+  // <div className=""></div>
+  return (elementGenerator({
+    element: 'div',
+    className: 'container mx-auto bg-green-500',
+    child
+  }))
+}
+
+export default Container
