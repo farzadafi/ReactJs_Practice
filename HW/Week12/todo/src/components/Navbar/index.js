a7bde833326b14c328f03888cada9fdae6313ff5
farzadafi <farzadafi40@gmail.com>
2023-03-06 16:11:25 +0330

    add plus icon

--- HW/Week12/todo/src/components/Navbar/index.js
+++ HW/Week12/todo/src/components/Navbar/index.js
@@ -1,17 +1,27 @@
 import elementGenerator from '@/library/elementGenerator.js'
-import { svgs } from '@/assets/svgs/index.js'
+import { svg } from '@/assets/svgs/index.js'
 
 const Navbar = () => {
   // <div className=""></div>
   return (elementGenerator({
     element: 'div',
-    className: 'flex p-2 bg-[#6200ea]',
+    className: 'flex p-2 bg-[#6200ea] justify-between',
     child: [
       elementGenerator({
         element: 'div',
         className: 'flex justify-center items-center gap-2 text-white',
         child: 'My To Do Tasks',
-        innerHTML: svgs.listUlIcon
+        innerHTML: svg.listUlIcon
+      }), elementGenerator({
+        element: 'div',
+        className: 'flex items-center',
+        child: [
+          elementGenerator({
+            element: 'button',
+            className: 'text-white',
+            innerHTML: svg.plusIcon
+          })
+        ]
       })
     ]
   }))
