270d7a80cb8b260d9ae6e25a3fc3d3682967c842
farzadafi <farzadafi40@gmail.com>
2023-03-06 16:51:14 +0330

    add new button for filter

--- HW/Week12/todo/src/components/Navbar/index.js
+++ HW/Week12/todo/src/components/Navbar/index.js
@@ -9,13 +9,18 @@ const Navbar = () => {
     child: [
       elementGenerator({
         element: 'div',
-        className: 'flex justify-center items-center gap-2 text-white',
+        className: 'flex justify-center items-center gap-2 text-white text-2xl',
         child: 'My To Do Tasks',
         innerHTML: svg.listUlIcon
       }), elementGenerator({
         element: 'div',
-        className: 'flex items-center',
+        className: 'flex items-center gap-6 mr-4',
         child: [
+          elementGenerator({
+            element: 'button',
+            className: 'text-white',
+            innerHTML: svg.filterIcon
+          }),
           elementGenerator({
             element: 'button',
             className: 'text-white',
