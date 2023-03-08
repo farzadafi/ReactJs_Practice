7c5113835db2d9d773b63202302f7842d9d33dda
farzadafi <farzadafi40@gmail.com>
2023-03-08 11:07:51 +0330

    add AddTask function

--- HW/Week12/todo/src/App.js
+++ HW/Week12/todo/src/App.js
@@ -1,11 +1,12 @@
 import Container from '@/layout/Container'
 import elementGenerator from '@/library/elementGenerator'
-import Navbar from "@/components/Navbar/index.js";
+import Navbar from '@/components/Navbar/index.js'
+import AddTask from '@/components/Modal/AddTask/index.js'
 
 const App = () => {
   const app = elementGenerator({
     element: 'div',
-    child: [Navbar(), ]
+    child: [Navbar(), AddTask()]
   })
   return Container(app)
 }
