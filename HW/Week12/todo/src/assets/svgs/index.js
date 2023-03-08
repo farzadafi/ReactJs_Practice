cfea36f54d5085acba3b1806af9fce8df3a443b3
farzadafi <farzadafi40@gmail.com>
2023-03-08 13:41:48 +0330

    add CaretDownFill svg

--- HW/Week12/todo/src/assets/svgs/index.js
+++ HW/Week12/todo/src/assets/svgs/index.js
@@ -7,5 +7,8 @@ export const svg = {
 </svg>`,
   filterIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-funnel-fill" viewBox="0 0 16 16">
   <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2z"/>
+</svg>`,
+  CaretDownFill: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
 </svg>`
 }
