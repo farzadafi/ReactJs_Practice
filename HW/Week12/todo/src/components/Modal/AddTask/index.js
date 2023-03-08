c9ca074650df81a2d66dea2a31dd374d2165cc1e
farzadafi <farzadafi40@gmail.com>
2023-03-08 11:26:58 +0330

    fix header tag and add one p tag

--- HW/Week12/todo/src/components/Modal/AddTask/index.js
+++ HW/Week12/todo/src/components/Modal/AddTask/index.js
@@ -7,12 +7,16 @@ const AddTask = () => {
     child: [
       elementGenerator({
         element: 'header',
-        className: 'm-4 text-2xl',
-        child: 'New Task'
-      }),
-      elementGenerator({
-        element: 'hr',
-        className: ''
+        child: [
+          elementGenerator({
+            element: 'p',
+            className: 'm-4 text-2xl',
+            child: 'New Task'
+          }),
+          elementGenerator({
+            element: 'hr'
+          })
+        ]
       }),
       elementGenerator({
         element: 'fieldset',
