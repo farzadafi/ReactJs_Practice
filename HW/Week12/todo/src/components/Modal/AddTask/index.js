9d1f22297bdd6b22f1d2c02ac5b40e106c173be3
farzadafi <farzadafi40@gmail.com>
2023-03-08 11:07:28 +0330

    add input tag into fieldset tag

--- HW/Week12/todo/src/components/Modal/AddTask/index.js
+++ HW/Week12/todo/src/components/Modal/AddTask/index.js
@@ -19,8 +19,17 @@ const AddTask = () => {
         className: 'border m-4 h-16 rounded',
         child: elementGenerator({
           element: 'legend',
-          className: 'ml-4 text-gray-100 p-1',
-          child: 'Task Name'
+          className: 'ml-4 text-gray-100 p-1 flex flex-col',
+          child: ['Task Name',
+            elementGenerator({
+              element: 'label',
+              for: 'taskName'
+            }),
+            elementGenerator({
+              element: 'input',
+              className: 'absolute top-[6.0rem] left-[1rem] w-[47.5rem] h-12 bg-opacity-0 p-5 bg-green-500 outline-none',
+              id: 'taskName'
+            })]
         })
       })
     ]
