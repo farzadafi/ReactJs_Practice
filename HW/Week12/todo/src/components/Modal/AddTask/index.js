e5901c90e4768bbccdc0ff38a18de1857b4e3581
farzadafi <farzadafi40@gmail.com>
2023-03-14 06:50:54 +0330

    fix onclick function cancel button

--- HW/Week12/todo/src/components/Modal/AddTask/index.js
+++ HW/Week12/todo/src/components/Modal/AddTask/index.js
@@ -269,7 +269,7 @@ const AddTask = () => {
                   const addTaskModal = document.getElementById('addTaskModal')
                   const container = document.getElementById('container')
                   addTaskModal.className = addTaskModal.className.replace('top-[20%]', 'top-[-90%]')
-                  container.className = container.className.replace('bg-opacity-30 bg-[black]', 'bg-white')
+                  container.className = container.className.replace('bg-opacity-30 bg-gray-900', 'bg-white')
                 },
                 variant: 'outlined'
               }),
