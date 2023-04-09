eabd02cd01ed70703b1f02e5e30a6d4e9b620e88
farzadafi <farzadafi40@gmail.com>
2023-04-09 18:16:40 +0330

    add new element to App function

--- HW/FinalJSProject/Shoea/src/App.js
+++ HW/FinalJSProject/Shoea/src/App.js
@@ -1,4 +1,12 @@
+import { Container } from '@/layout'
+import { ElementGenerator } from '@/library'
+import { LoadingPage } from '@/screens'
+
 const App = () => {
+  return ElementGenerator({
+    element: 'div',
+    child: [Container(LoadingPage())]
+  })
 }
 
 export default App
