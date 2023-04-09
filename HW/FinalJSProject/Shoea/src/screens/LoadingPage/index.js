797e61cd7c62c31a3037a18bcd9fa340e90b3ed6
farzadafi <farzadafi40@gmail.com>
2023-04-09 18:14:37 +0330

    add a new div for svg

--- HW/FinalJSProject/Shoea/src/screens/LoadingPage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/LoadingPage/index.js
@@ -1,14 +1,20 @@
 import { ElementGenerator } from '@/library'
+import { svg } from '@/assets'
 
 export const LoadingPage = () => {
   return ElementGenerator({
     element: 'div',
-    className: 'bg-orange-400 w-[430px] h-[926px] flex flex-col justify-center items-center',
+    className: 'w-screen h-[926px] flex flex-col justify-center items-center',
     child: [
       ElementGenerator({
         element: 'img',
         className: 'w-[231px] h-[63px]',
         src: './src/assets/image/ShoeaIconWithWord.png'
+      }),
+      ElementGenerator({
+        element: 'div',
+        className: 'absolute top-1/3 transform translate-y-96',
+        innerHTML: svg.loadingSvg
       })
     ]
   })
