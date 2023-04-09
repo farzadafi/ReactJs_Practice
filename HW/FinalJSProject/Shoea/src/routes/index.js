c2e5d1332b9a4b5c60748b8ceffa02697ab37e75
farzadafi <farzadafi40@gmail.com>
2023-04-09 20:42:14 +0330

    add Routes function

---
+++ HW/FinalJSProject/Shoea/src/routes/index.js
@@ -0,0 +1,13 @@
+import { ElementGenerator } from '@/library/index.js'
+import { WelcomePage, LoadingPage } from '@/screens/index.js'
+
+export const Routes = () => {
+  const routes = document.getElementById('routes') || ElementGenerator({ element: 'div' })
+  routes.innerHTML = ''
+  switch (location.pathname) {
+    case '/' :
+      return routes.appendChild(LoadingPage())
+    default :
+      return routes.innerHTML = 'gom shodi!'
+  }
+}
