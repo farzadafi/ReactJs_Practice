e44da626eeafb09412235478e2e039cd4a5b4604
farzadafi <farzadafi40@gmail.com>
2023-04-09 19:50:08 +0330

    add a function for create Welcome page

---
+++ HW/FinalJSProject/Shoea/src/screens/WelcomePage/index.js
@@ -0,0 +1,10 @@
+import { ElementGenerator } from '@/library'
+
+export const WelcomePage = () => {
+  return ElementGenerator({
+    element: 'div',
+    className: 'w-screen h-[926px] bg-[url(./src/assets/image/welcomePageBackground.png)]',
+    child: [
+    ]
+  })
+}
