4dcfb8e5dfa4d1ba51c99984d713e88d0e4f9878
farzadafi <farzadafi40@gmail.com>
2023-04-09 19:56:34 +0330

    create a div for end content

--- HW/FinalJSProject/Shoea/src/screens/WelcomePage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/WelcomePage/index.js
@@ -3,8 +3,13 @@ import { ElementGenerator } from '@/library'
 export const WelcomePage = () => {
   return ElementGenerator({
     element: 'div',
-    className: 'w-screen h-[926px] bg-[url(./src/assets/image/welcomePageBackground.png)]',
+    className: 'w-screen h-[926px] bg-[url(./src/assets/image/welcomePageBackground.png)] flex justify-end flex-col',
     child: [
+      ElementGenerator({
+        element: 'div',
+        className: 'bg-red-400',
+        child: 'div'
+      })
     ]
   })
 }
