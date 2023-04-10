f69ac53d3e746dece0d18f24611b5dc37c530172
farzadafi <farzadafi40@gmail.com>
2023-04-10 17:32:34 +0330

    fix App function

--- HW/FinalJSProject/Shoea/src/App.js
+++ HW/FinalJSProject/Shoea/src/App.js
@@ -1,12 +1,18 @@
 import { Container } from '@/layout'
 import { ElementGenerator } from '@/library'
-import { LoadingPage } from '@/screens'
+import { LoadingPage, WelcomePage } from '@/screens'
 
 const App = () => {
-  return ElementGenerator({
+  const app = ElementGenerator({
     element: 'div',
     child: [Container(LoadingPage())]
   })
+  setTimeout(function () {
+    const loading = app.childNodes[0]
+    app.removeChild(loading)
+    app.appendChild(WelcomePage())
+  }, 3000)
+  return app
 }
 
 export default App
