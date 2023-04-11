4b6776051f924403ae5181eb24c7f00e49a72b02
farzadafi <farzadafi40@gmail.com>
2023-04-11 08:58:53 +0330

    fix className and remove grid class

--- HW/FinalJSProject/Shoea/src/layout/Container/index.js
+++ HW/FinalJSProject/Shoea/src/layout/Container/index.js
@@ -3,7 +3,7 @@ import { ElementGenerator } from '@/library'
 export const Container = (child) => {
   return (ElementGenerator({
     element: 'div',
-    className: ' mx-auto bg-white grid justify-center dark:bg-gray-700',
+    className: ' mx-auto bg-white justify-center dark:bg-gray-700',
     child
   }))
 }
