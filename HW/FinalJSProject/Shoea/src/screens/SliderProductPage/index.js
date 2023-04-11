e1a8f7a846e94dad0139be063501227003ff7bd9
farzadafi <farzadafi40@gmail.com>
2023-04-11 09:20:08 +0330

    add first image

--- HW/FinalJSProject/Shoea/src/screens/SliderProductPage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/SliderProductPage/index.js
@@ -17,6 +17,21 @@ export const SliderProductPage = () => {
             element: 'div',
             className: 'relative h-56 overflow-hidden rounded-lg md:h-96',
             child: [
+              ElementGenerator({
+                element: 'div',
+                className: 'hidden duration-700 ease-in-out',
+                restAttrs: {
+                  'data-carousel-item': ''
+                },
+                child: [
+                  ElementGenerator({
+                    element: 'img',
+                    src: './src/assets/image/SliderFirstImage.png',
+                    className: 'absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2',
+                    alt: '...'
+                  })
+                ]
+              })
             ]
           })
         ]
