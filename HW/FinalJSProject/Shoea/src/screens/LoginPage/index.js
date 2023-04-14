d4f74d5b1c113dc17265fb95adab96d8793d0f98
farzadafi <farzadafi40@gmail.com>
2023-04-14 12:19:07 +0330

    create image tag in header

--- HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
@@ -3,12 +3,19 @@ import { ElementGenerator } from '@/library'
 export const LoginPage = () => {
   return ElementGenerator({
     element: 'div',
-    className: 'h-[926px] bg-gray-300',
+    className: 'h-[926px] bg-gray-300 p-4',
     child: [
       ElementGenerator({
         element: 'header',
         className: '',
-        child: ''
+        child: [
+          ElementGenerator({
+            element: 'img',
+            className: '',
+            src: './src/assets/image/arrow-left-short.png',
+            onclick: () => { alert('last page') }
+          })
+        ]
       })
     ]
   })
