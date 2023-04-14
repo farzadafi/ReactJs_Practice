0aa402c6c1c6ff0c7f9ac88a8142607c93274918
farzadafi <farzadafi40@gmail.com>
2023-04-14 13:07:09 +0330

    fix id for email input

--- HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
@@ -4,7 +4,7 @@ import { svg } from '@/assets/index.js'
 export const LoginPage = () => {
   return ElementGenerator({
     element: 'div',
-    className: 'h-[926px] bg-gray-300 p-4',
+    className: 'h-[926px] p-4',
     child: [
       ElementGenerator({
         element: 'header',
@@ -46,7 +46,7 @@ export const LoginPage = () => {
                     child: [
                       ElementGenerator({
                         element: 'label',
-                        htmlFor: 'email-address-icon',
+                        htmlFor: 'email-address-id',
                         className: 'block mb-2 text-sm font-medium text-gray-900 dark:text-white'
                       }),
                       ElementGenerator({
@@ -55,7 +55,7 @@ export const LoginPage = () => {
                         child: [
                           ElementGenerator({
                             element: 'div',
-                            className: 'absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none',
+                            className: 'absolute inset-y-6 left-0 flex items-center pl-3 pointer-events-none',
                             innerHTML: svg.emailSvg
                           })
                         ]
@@ -63,8 +63,8 @@ export const LoginPage = () => {
                       ElementGenerator({
                         element: 'input',
                         type: 'text',
-                        id: 'email-address-icon',
-                        className: 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                        id: 'email-address-id',
+                        className: 'bg-gray-50 border-none text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5',
                         placeholder: 'Email'
                       })
                     ]
