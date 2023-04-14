b3a187363d7ca82e32acac7be680405ffe4970a7
farzadafi <farzadafi40@gmail.com>
2023-04-14 20:37:01 +0330

    fix remember me checked mood

--- HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
@@ -108,7 +108,7 @@ export const LoginPage = () => {
               }),
               ElementGenerator({
                 element: 'div',
-                className: 'flex items-start mb-6',
+                className: 'flex items-start mb-6 justify-center',
                 child: [
                   ElementGenerator({
                     element: 'div',
@@ -119,7 +119,7 @@ export const LoginPage = () => {
                         id: 'remember',
                         type: 'checkbox',
                         value: '',
-                        className: 'w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800'
+                        className: 'w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:bg-black focus:ring-black checked:bg-black'
                       })
                     ]
                   }),
