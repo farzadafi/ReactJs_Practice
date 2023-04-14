1774349352cb8567c1b618e7b8bafe981fe2f623
farzadafi <farzadafi40@gmail.com>
2023-04-14 12:54:45 +0330

    create input for email address

--- HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
@@ -1,4 +1,5 @@
 import { ElementGenerator } from '@/library'
+import { svg } from '@/assets/index.js'
 
 export const LoginPage = () => {
   return ElementGenerator({
@@ -34,6 +35,41 @@ export const LoginPage = () => {
                 element: 'h1',
                 className: 'font-semibold text-4xl',
                 child: 'Login to Your Account'
+              }),
+              ElementGenerator({
+                element: 'div',
+                className: '',
+                child: [
+                  ElementGenerator({
+                    element: 'div',
+                    className: '',
+                    child: [
+                      ElementGenerator({
+                        element: 'label',
+                        htmlFor: 'email-address-icon',
+                        className: 'block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                      }),
+                      ElementGenerator({
+                        element: 'div',
+                        className: 'relative',
+                        child: [
+                          ElementGenerator({
+                            element: 'div',
+                            className: 'absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none',
+                            innerHTML: svg.emailSvg
+                          })
+                        ]
+                      }),
+                      ElementGenerator({
+                        element: 'input',
+                        type: 'text',
+                        id: 'email-address-icon',
+                        className: 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                        placeholder: 'Email'
+                      })
+                    ]
+                  })
+                ]
               })
             ]
           })
