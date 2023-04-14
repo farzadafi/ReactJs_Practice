5c88229c31ff57b72c03219606ac954d9ec97918
farzadafi <farzadafi40@gmail.com>
2023-04-14 09:39:06 +0330

    create LoginPage function

---
+++ HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
@@ -0,0 +1,10 @@
+import { ElementGenerator } from '@/library'
+
+export const LoginPage = () => {
+  return ElementGenerator({
+    element: 'div',
+    className: 'h-[926px] bg-gray-300',
+    child: [
+    ]
+  })
+}
