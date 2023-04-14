27b4bc526cdd3a05a5a895b54ce9a3d999945e87
farzadafi <farzadafi40@gmail.com>
2023-04-14 13:51:26 +0330

    add remember me

--- HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/LoginPage/index.js
@@ -105,6 +105,31 @@ export const LoginPage = () => {
                     ]
                   })
                 ]
+              }),
+              ElementGenerator({
+                element: 'div',
+                className: 'flex items-start mb-6',
+                child: [
+                  ElementGenerator({
+                    element: 'div',
+                    className: 'flex items-center h-5',
+                    child: [
+                      ElementGenerator({
+                        element: 'input',
+                        id: 'remember',
+                        type: 'checkbox',
+                        value: '',
+                        className: 'w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800'
+                      })
+                    ]
+                  }),
+                  ElementGenerator({
+                    element: 'label',
+                    htmlFor: 'remember',
+                    className: 'ml-2 text-sm font-medium text-gray-900 dark:text-gray-300',
+                    innerHTML: 'Remember me'
+                  })
+                ]
               })
             ]
           })
