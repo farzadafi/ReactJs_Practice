18b85c6dcfff9a5c79b008115655f029cf3c2b82
farzadafi <farzadafi40@gmail.com>
2023-04-15 08:54:43 +0330

    add image profile section

--- HW/FinalJSProject/Shoea/src/screens/FirstProductPage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/FirstProductPage/index.js
@@ -3,8 +3,31 @@ import { ElementGenerator } from '@/library'
 export const FirstProductPage = () => {
   return ElementGenerator({
     element: 'div',
-    className: 'h-[926px]',
+    className: 'h-[926px] flex flex-col p-5',
     child: [
+      ElementGenerator({
+        element: 'div',
+        className: '',
+        child: [
+          ElementGenerator({
+            element: 'div',
+            className: '',
+            child: [
+              ElementGenerator({
+                element: 'div',
+                className: '',
+                child: [
+                  ElementGenerator({
+                    element: 'img',
+                    className: 'w-12 rounded-full',
+                    src: './src/assets/image/farzadProf.jpg'
+                  })
+                ]
+              })
+            ]
+          })
+        ]
+      })
     ]
   })
 }
