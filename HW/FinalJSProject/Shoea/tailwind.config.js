d13332a636771cf272b053ac8f803763c7b6676e
farzadafi <farzadafi40@gmail.com>
2023-04-15 08:39:13 +0330

    add flowbite property

--- HW/FinalJSProject/Shoea/tailwind.config.js
+++ HW/FinalJSProject/Shoea/tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   content: [
     './index.html',
     './main.js',
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {}
   },
   plugins: [
+    require('./node_modules/flowbite/plugin.js')
   ]
 }
