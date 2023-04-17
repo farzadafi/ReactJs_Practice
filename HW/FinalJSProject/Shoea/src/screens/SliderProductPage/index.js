77b65bcac9a7d6f8fa2a820395827bcb065375b0
farzadafi <farzadafi40@gmail.com>
2023-04-17 06:26:11 +0330

    add const slides

--- HW/FinalJSProject/Shoea/src/screens/SliderProductPage/index.js
+++ HW/FinalJSProject/Shoea/src/screens/SliderProductPage/index.js
@@ -1,4 +1,19 @@
 import { ElementGenerator } from '@/library'
+import Swiper from 'swiper/bundle'
+
+const slides = [
+  {
+    text: 'We provide high quality products just for you'
+  },
+  {
+    text: 'Your satisfaction is our number one priority',
+    src: './src/assets/images/SliderSecondImage.png'
+  },
+  {
+    text: 'Let’s fulfill your fashion needs with shoea right now!',
+    src: './src/assets/images/SliderThirdImage.png'
+  }
+]
 
 export const SliderProductPage = () => {
   return ElementGenerator({
