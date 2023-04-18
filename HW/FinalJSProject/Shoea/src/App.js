c73c95ada382e45bc5bd44189aa5f6802de18cdb
farzadafi <farzadafi40@gmail.com>
2023-04-18 09:38:50 +0330

    fix App function and add new setTimeout

--- HW/FinalJSProject/Shoea/src/App.js
+++ HW/FinalJSProject/Shoea/src/App.js
@@ -1,6 +1,6 @@
 import { Container } from '@/layout'
 import { ElementGenerator } from '@/library'
-import { LoadingPage, WelcomePage } from '@/screens'
+import { LoadingPage, WelcomePage, SliderProductPage } from '@/screens'
 
 const App = () => {
   const app = ElementGenerator({
@@ -12,6 +12,11 @@ const App = () => {
     app.removeChild(loading)
     app.appendChild(WelcomePage())
   }, 3000)
+  setTimeout(function () {
+    const welcome = app.childNodes[0]
+    app.removeChild(welcome)
+    app.appendChild(SliderProductPage())
+  }, 6000)
   return app
 }
 
