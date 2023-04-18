ed2a58b07b428f792fbae0a5a2a5abb18eec0f7c
farzadafi <farzadafi40@gmail.com>
2023-04-18 10:42:48 +0330

    fix plugins

--- practice/scss/scss-practice/vite.config.js
+++ practice/scss/scss-practice/vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import sass from 'sass';
 
 export default defineConfig({
-    plugins: [vue(), {
+    plugins: [{
         name: 'sass',
         preprocess: {
-            // Use `dart-sass`
-            scss: {
-                implementation: sass,
-            },
-            // Add variables and mixins to all style blocks
-            // that are processed by the `vue-loader`
             'scss': {
                 additionalData: '@import "./src/style/variables.scss";@import "./src/style/styles.scss";',
             },
