72bac1535dd5b7f2de912144ae1eb239346fe942
farzadafi <farzadafi40@gmail.com>
2023-04-20 17:12:22 +0330

    create Main

--- CW/Week17/src/Component/Main/index.tsx
+++ CW/Week17/src/Component/Main/index.tsx
@@ -1,22 +1,17 @@
+import Section1 from "./Component/Section1";
+import Section2 from "./Component/Section2";
+
 function Main() {
     return (<main className={""}>
-            <div className={"w-full flex"}>
-                <div className={"bg-[#5b5bff] flex justify-end gap-12 items-center flex-col h-64 p-10 text-white flex-1"}>
-                    <p>بلاگفا یک ابزار قدرتمند برای ساخت و مدیریت وبلاگ است . بلاگفا به شما کمک میکند تا با سرعت و سهولت
-                        اطلاعات ، خاطرات و مطالب و مقالات خود را در اینترنت منتشر کنید</p>
-                    <button className={"bg-[#0101aa] p-1"}>
-                        ثبت نام و ایجاد وبلاگ جدید
-                    </button>
+            <Section1/>
+            <div className={"flex items-center gap-3 mt-8"}>
+                <div className={"flex-1 h-[2px] bg-slate-300"}>
                 </div>
-                <div className={"bg-blue-500 p-4 bg-gray-200"}>
-                    <form className={"flex flex-col w-96 gap-5"}>
-                        <input className={"placeholder:text-center h-10 border border-slate-500"} placeholder={"نام کاربری"}/>
-                        <input className={"placeholder:text-center h-10 border border-slate-500"} placeholder={"رمز عبور"}/>
-                        <input className={"bg-[#4444ff] py-2 text-white"} value={"ورود به بخش مدیریت وبلاگ"} type="submit"/>
-                        <a className={"text-center text-[#4444ff] text-sm mt-4"} href="/">کلمه عبور خود را فراموش کرده ام</a>
-                    </form>
+                <p>وبلاگ های بروز شده</p>
+                <div className={"flex-1 h-[2px] bg-slate-300"}>
                 </div>
             </div>
+            <Section2/>
         </main>
     )
 }
