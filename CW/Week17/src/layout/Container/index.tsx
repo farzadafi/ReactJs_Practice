c0f4c4ca36f49259d7df4cc47e457c512d1bac71
farzadafi <farzadafi40@gmail.com>
2023-04-20 17:11:11 +0330

    fix class name

--- CW/Week17/src/layout/Container/index.tsx
+++ CW/Week17/src/layout/Container/index.tsx
@@ -4,7 +4,7 @@ type containerProps = {
     children:React.ReactNode
 }
 function Container({children}:containerProps) {
-    return ( <div className={"container mx-auto max-w-7xl mt-4"}>
+    return ( <div className={"container mx-auto max-w-[120rem] mt-2"}>
         {children}
     </div>)
 }
