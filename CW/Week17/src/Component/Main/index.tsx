2f3fd73abed64660750dee98ae8a4a42f0905acc
farzadafi <farzadafi40@gmail.com>
2023-04-20 15:09:31 +0330

    create first div

---
+++ CW/Week17/src/Component/Main/index.tsx
@@ -0,0 +1,19 @@
+function Main() {
+    return (<main className={""}>
+            <div className={"w-full"}>
+                <div className={"bg-[#5b5bff] flex justify-end gap-12 items-center flex-col h-64 p-10 text-white"}>
+                    <p>بلاگفا یک ابزار قدرتمند برای ساخت و مدیریت وبلاگ است . بلاگفا به شما کمک میکند تا با سرعت و سهولت
+                        اطلاعات ، خاطرات و مطالب و مقالات خود را در اینترنت منتشر کنید</p>
+                    <button className={"bg-[#0101aa] p-1"}>
+                        ثبت نام و ایجاد وبلاگ جدید
+                    </button>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main
