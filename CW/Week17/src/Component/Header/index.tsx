979c66139f5248a8f66404a8c9a6744b868a8672
farzadafi <farzadafi40@gmail.com>
2023-04-22 15:12:23 +0330

    move all project in dir cakeKadoyi

--- CW/Week17/src/Component/Header/index.tsx
+++
@@ -1,9 +0,0 @@
-// import './App.css'
-
-function Header() {
-    return ( <header>
-        <img alt="blogfa image" src={"./image/blogfa.jpg"}/>
-    </header>)
-}
-
-export default Header
--- CW/Week17/src/Component/InputMaker/InputMaker.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const InputMaker= ({placeholder, type, className, value}:React.InputHTMLAttributes<any>) => {
-    return (
-        <input placeholder={placeholder} type={type} className={className} value={value}/>
-    );
-};
-
-export default InputMaker
