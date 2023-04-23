4e0d00f5c28d6976cc918ceff4ab7be5132101eb
farzadafi <farzadafi40@gmail.com>
2023-04-23 09:51:41 +0330

    create Header

---
+++ CW/Week17/mcDonald/src/Component/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className={"flex justify-center py-4 border-b-2"}>
+      <p> رستوران مک دونالد شعبه تهران</p>
+    </div>
+  );
+};
+
+export default Header;
