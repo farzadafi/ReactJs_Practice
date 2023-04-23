ceedd15357fc63203aad79582fa8c724fd8c27bb
farzadafi <farzadafi40@gmail.com>
2023-04-23 14:52:50 +0330

    add Basket function

---
+++ CW/Week17/mcDonald/src/Component/Basket/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "../Button";
+
+const tick:JSX.Element = <img alt={"tick"} src={"../../../public/image/tick.png"} className={"w-8 p-2"}/>
+const register: JSX.Element = <p>ثبت سفارش</p>
+
+const Basket = () => {
+  return (
+    <div className={"flex flex-col w-full p-4 rtl-dir rounded-xl gap-2 bg-orange-500"}>
+      <div className={"flex justify-between text-gray-500"}>
+        <p>جمع کل سفارشات:</p>
+        <p className={"direction-ltr text-right"}>۰ تومان</p>
+      </div>
+      <div className={"flex justify-between text-gray-500"}>
+        <p>حق سرویس و کارمزد:</p>
+        <p className={"direction-ltr text-right"}>۰ تومان</p>
+      </div>
+      <div className={"flex justify-between text-gray-500"}>
+        <p>تخفیف:</p>
+        <p className={"direction-ltr text-right"}>۰ تومان</p>
+      </div>
+      <div>
+        <input className={"w-full h-8 p-2 rounded-md"} placeholder={"کد تخفیف"} type="text"/>
+        <Button variant={"tick"} classes={""} child={tick}/>
+      </div>
+      <div className={"flex justify-between mt-5 bg-yellow-400 p-2 rounded-md"}>
+        <p>مبلغ قابل پرداخت:</p>
+        <p>۰ تومان</p>
+      </div>
+      <Button variant={"pay"} classes={""} child={register}/>
+    </div>
+  );
+};
+
+export default Basket;
