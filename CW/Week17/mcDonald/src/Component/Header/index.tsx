610d0fb25d83d83eefc6ebf4f0c921b58612bfcd
farzadafi <farzadafi40@gmail.com>
2023-04-23 13:27:21 +0330

    fix header class

--- CW/Week17/mcDonald/src/Component/Header/index.tsx
+++ CW/Week17/mcDonald/src/Component/Header/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className={"flex justify-center py-4 border-b-2"}>
+    <div className={"flex justify-center py-4 border-b-4 border-gray-300 bg-gray-100 rounded-t-xl"}>
       <p> رستوران مک دونالد شعبه تهران</p>
     </div>
   );
