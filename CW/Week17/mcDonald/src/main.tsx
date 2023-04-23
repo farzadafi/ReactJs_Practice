e57263e032e82cd1d3f5cc958f76417a6d1e0716
farzadafi <farzadafi40@gmail.com>
2023-04-23 09:38:11 +0330

    import css main file

--- CW/Week17/mcDonald/src/main.tsx
+++ CW/Week17/mcDonald/src/main.tsx
@@ -1,6 +1,7 @@
 import App from "./App";
 import ReactDOM from 'react-dom/client'
 import React from 'react'
+import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
