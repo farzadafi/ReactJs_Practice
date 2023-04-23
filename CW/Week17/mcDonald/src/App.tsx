6d2fdd96cd0d121c6859ad88dde0e10044c93b2a
farzadafi <farzadafi40@gmail.com>
2023-04-23 09:22:41 +0330

    crate App function

---
+++ CW/Week17/mcDonald/src/App.tsx
@@ -0,0 +1,13 @@
+import Container from "./layout/Container";
+
+const App = () => {
+  return (
+    <div>
+      <Container>
+        <p>farzad</p>
+      </Container>
+    </div>
+  );
+};
+
+export default App;
