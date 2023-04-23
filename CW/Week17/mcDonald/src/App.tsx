7447c78d90ad5de56eaf78e19d69c5e29100d57d
farzadafi <farzadafi40@gmail.com>
2023-04-23 10:25:31 +0330

    add main to App

--- CW/Week17/mcDonald/src/App.tsx
+++ CW/Week17/mcDonald/src/App.tsx
@@ -1,11 +1,13 @@
 import Container from "./layout/Container";
 import Header from "./Component/Header";
+import Main from "./Component/Main";
 
 const App = () => {
   return (
     <div className={"font-Vazir"}>
       <Container>
         <Header/>
+        <Main/>
       </Container>
     </div>
   );
