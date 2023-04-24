75d856e36d10eb7c065ab88b63c18d5a5b997af5
farzadafi <farzadafi40@gmail.com>
2023-04-24 12:23:40 +0330

    add new quantityNumber for berger array

--- CW/Week17/mcDonald/src/Component/Main/index.tsx
+++ CW/Week17/mcDonald/src/Component/Main/index.tsx
@@ -7,61 +7,71 @@ const berger = [
     id: 1,
     bergerName: "همبرگر معمولی",
     price: "۸۰۰۰ تومان",
-    image: "./public/image/hamburger.png"
+    image: "./public/image/hamburger.png",
+    quantityNumber : 0
   },
   {
     id: 1,
     bergerName: "همبرگر مخصوص",
     price: "۱۰۰۰۰ تومان",
-    image: "./public/image/hamburger.png"
+    image: "./public/image/hamburger.png",
+    quantityNumber : 0
   },
   {
     id: 1,
     bergerName: "همبرگر معمولی با قارچ و پنیر",
     price: "۱۰۰۰۰ تومان",
-    image: "./public/image/hamburger.png"
+    image: "./public/image/hamburger.png",
+    quantityNumber : 0
   },
   {
     id: 1,
     bergerName: "همبرگر مخصوص با قارچ و پنیر",
     price: "۲۰۰۰۰ تومان",
-    image: "./public/image/hamburger.png"
+    image: "./public/image/hamburger.png",
+    quantityNumber : 0
   },
   {
     id: 1,
     bergerName: "سیب زمینی سرخ کرده ویژه",
     price: "۲۵۰۰۰ تومان",
-    image: "./public/image/french_fries.png"
+    image: "./public/image/french_fries.png",
+    quantityNumber : 0
   },
   {
     id: 1,
     bergerName: "سیب زمینی سرخ کرده",
     price: "۱۰۰۰۰ تومان",
-    image: "./public/image/french_fries.png"
+    image: "./public/image/french_fries.png",
+    quantityNumber : 0
   },
   {
     id: 1,
     bergerName: "نوشابه رژیمی",
     price: "۶۰۰۰ تومان",
-    image: "./public/image/soda.png"
+    image: "./public/image/soda.png",
+    quantityNumber : 0
   },
   {
     id: 1,
     bergerName: "نوشابه ",
     price: "۵۰۰۰ تومان",
-    image: "./public/image/soda.png"
+    image: "./public/image/soda.png",
+    quantityNumber : 0
   },
   {
     id: 1,
     bergerName: "سالاد فصل ",
     price: "۸۰۰۰ تومان",
-    image: "./public/image/salad.png"
+    image: "./public/image/salad.png",
+    quantityNumber : 0
   },
   {
     id: 1,
     bergerName: "سالاد سزار ",
     price: "۲۵۰۰۰ تومان",
-    image: "./public/image/ceasar.png"
+    image: "./public/image/ceasar.png",
+    quantityNumber : 0
   },
 ];
 
@@ -69,8 +79,8 @@ const Main = () => {
   return (
     <main className={"flex justify-center flex-wrap"}>
       {
-        berger.map(({bergerName, price, image}) => {
-          return <Card name={bergerName} price={price} image={image}/>;
+        berger.map(({bergerName, price, image, quantityNumber}) => {
+          return <Card name={bergerName} price={price} image={image} quantity={quantityNumber}/>;
         })
       }
     <Basket/>
