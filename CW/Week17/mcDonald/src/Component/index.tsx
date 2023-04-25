c597407619cddfc3bdade8d01f4d9e8fff10bb23
farzadafi <farzadafi40@gmail.com>
2023-04-25 21:00:04 +0330

    fix increment and decrement section and calculate services price

--- CW/Week17/mcDonald/src/Component/index.tsx
+++ CW/Week17/mcDonald/src/Component/index.tsx
@@ -2,10 +2,12 @@ import {createStore} from "redux";
 
 type AppState = {
   price: number;
+  services: number
 };
 
 const initialState: AppState = {
   price: 0,
+  services: 0
 };
 
 type IncrementAction = {
@@ -18,14 +20,28 @@ type DecrementAction = {
   decrementPayload: number;
 }
 
+let result:number = 0
+
 function reducer(state = initialState, action: any) {
+  let price = state.price
+  let finalPrice : number;
+
   switch (action.type) {
-  case "INCREMENT":
+  case "INCREMENT": {
     const {incrementPayload} = action as IncrementAction;
-    return {...state, price: state.price + incrementPayload};
+    finalPrice = price + incrementPayload
+    if( finalPrice == 0 )
+      result = incrementPayload * 0.05
+    else
+      result = finalPrice * 0.05
+    return {...state, price: finalPrice, services: result};
+  }
     case "DECREMENT":
       const {decrementPayload} = action as DecrementAction;
-      return {...state, price: state.price - decrementPayload};
+      finalPrice = price - decrementPayload
+      result = finalPrice * 0.05
+      price = state.price
+      return {...state, price: finalPrice, services: result};
   default:
     return state;
   }
