f4e626e6f644a1888e021a435ba817995eb73b5a
farzadafi <farzadafi40@gmail.com>
2023-04-25 20:59:39 +0330

    fix services price p tag

--- CW/Week17/mcDonald/src/Component/Basket/index.tsx
+++ CW/Week17/mcDonald/src/Component/Basket/index.tsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 
 type AppState = {
   price: number;
+  services: number;
 };
 
 const tick:JSX.Element = <img alt={"tick"} src={"../../../public/image/tick.png"} className={"w-8 p-2"}/>
@@ -11,6 +12,7 @@ const register: JSX.Element = <p>ثبت سفارش</p>
 
 const Basket = () => {
   const price = useSelector((state: AppState) => state.price);
+  const services = useSelector((state: AppState) => state.services);
   return (
     <div className={"flex flex-col w-full p-4 rtl-dir rounded-xl gap-2 bg-gray-200 m-4 max-w-xs"}>
       <div className={"flex justify-between text-gray-500"}>
@@ -22,7 +24,7 @@ const Basket = () => {
       </div>
       <div className={"flex justify-between text-gray-500"}>
         <p>حق سرویس و کارمزد:</p>
-        <p className={"direction-ltr text-right"}>۰ تومان</p>
+        <p className={"direction-ltr text-right"}>{services} تومان</p>
       </div>
       <div className={"flex justify-between text-gray-500"}>
         <p>تخفیف:</p>
