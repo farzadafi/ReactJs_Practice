0f1aaed1517c5869444a779ea2ecc70859f914d0
farzadafi <farzadafi40@gmail.com>
2023-04-25 09:21:10 +0330

    fix count of all buy price

--- CW/Week17/mcDonald/src/Component/Basket/index.tsx
+++ CW/Week17/mcDonald/src/Component/Basket/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Button from "../Button";
+import { useSelector } from "react-redux";
+
+type AppState = {
+  price: number;
+};
 
 const tick:JSX.Element = <img alt={"tick"} src={"../../../public/image/tick.png"} className={"w-8 p-2"}/>
 const register: JSX.Element = <p>ثبت سفارش</p>
 
 const Basket = () => {
+  const price = useSelector((state: AppState) => state.price);
   return (
     <div className={"flex flex-col w-full p-4 rtl-dir rounded-xl gap-2 bg-gray-200 m-4 max-w-xs"}>
       <div className={"flex justify-between text-gray-500"}>
         <p>جمع کل سفارشات:</p>
-        <p className={"direction-ltr text-right"}>۰ تومان</p>
+        <div className={"flex gap-1"}>
+          <p>{price}</p>
+          <p className={"direction-ltr text-right"}> تومان</p>
+        </div>
       </div>
       <div className={"flex justify-between text-gray-500"}>
         <p>حق سرویس و کارمزد:</p>
