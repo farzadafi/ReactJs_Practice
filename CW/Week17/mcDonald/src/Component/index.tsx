897bf27bc5fdf048723fae24746bc15136711676
farzadafi <farzadafi40@gmail.com>
2023-04-25 21:04:47 +0330

    fix increment and decrement case

--- CW/Week17/mcDonald/src/Component/index.tsx
+++ CW/Week17/mcDonald/src/Component/index.tsx
@@ -20,28 +20,24 @@ type DecrementAction = {
   decrementPayload: number;
 }
 
-let result:number = 0
 
 function reducer(state = initialState, action: any) {
-  let price = state.price
-  let finalPrice : number;
+  let price = state.price;
+  let finalPrice: number;
+  let result: number;
 
   switch (action.type) {
   case "INCREMENT": {
     const {incrementPayload} = action as IncrementAction;
-    finalPrice = price + incrementPayload
-    if( finalPrice == 0 )
-      result = incrementPayload * 0.05
-    else
-      result = finalPrice * 0.05
+    finalPrice = price + incrementPayload;
+    result = finalPrice * 0.05;
     return {...state, price: finalPrice, services: result};
   }
-    case "DECREMENT":
-      const {decrementPayload} = action as DecrementAction;
-      finalPrice = price - decrementPayload
-      result = finalPrice * 0.05
-      price = state.price
-      return {...state, price: finalPrice, services: result};
+  case "DECREMENT":
+    const {decrementPayload} = action as DecrementAction;
+    finalPrice = price - decrementPayload;
+    result = finalPrice * 0.05;
+    return {...state, price: finalPrice, services: result};
   default:
     return state;
   }
