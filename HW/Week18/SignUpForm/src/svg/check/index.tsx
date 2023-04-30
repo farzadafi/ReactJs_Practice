7575f71bf7d80fc9c5dc23adbbf68b754c383cc7
farzadafi <farzadafi40@gmail.com>
2023-04-30 07:34:22 +0330

    fix size and color

--- HW/Week18/SignUpForm/src/svg/check/index.tsx
+++ HW/Week18/SignUpForm/src/svg/check/index.tsx
@@ -1,6 +1,6 @@
 const CheckSvg = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check2-circle"
+    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="gray" className="bi bi-check2-circle"
          viewBox="0 0 16 16">
       <path
         d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
