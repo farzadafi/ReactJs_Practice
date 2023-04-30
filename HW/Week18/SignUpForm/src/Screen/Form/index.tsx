202dfebf8779998cc2b931bb8d66c1b16f873bde
farzadafi <farzadafi40@gmail.com>
2023-04-30 07:39:50 +0330

    add new button and fix class for main div

--- HW/Week18/SignUpForm/src/Screen/Form/index.tsx
+++ HW/Week18/SignUpForm/src/Screen/Form/index.tsx
@@ -8,8 +8,8 @@ const confirmPassword = <CheckSvg/>
 
 const Form = () => {
   return (
-    <div className={"w-48 h-52 bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-4"}>
-      <div className={"flex flex-col gap-2"}>
+    <div className={"w-48 h-64 bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-4"}>
+      <div className={"flex flex-col gap-4"}>
         <div className={"flex gap-2 px-6"}>
           <Button title={"Sign In"} classes={""}/>
           <Button title={"Sign Up"} classes={"bg-purple-600 font-white"}/>
@@ -18,6 +18,7 @@ const Form = () => {
         <Input placeHolder={"Username"} svg={People} name={"username"} type={"text"}/>
         <Input placeHolder={"password"} svg={password} name={"password"} type={"password"}/>
         <Input placeHolder={"confirm password"} svg={confirmPassword} name={"confirmPassword"} type={"password"}/>
+        <Button title={"Sign Up"} classes={"bg-purple-600 text-white"}/>
       </div>
     </div>
   );
