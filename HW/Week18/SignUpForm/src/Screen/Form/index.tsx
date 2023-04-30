9377854225da80aefbfa26c276c0cc465fa0dd36
farzadafi <farzadafi40@gmail.com>
2023-04-30 05:36:01 +0330

    create main div form

---
+++ HW/Week18/SignUpForm/src/Screen/Form/index.tsx
@@ -0,0 +1,9 @@
+const Form = () => {
+  return (
+    <div className={"w-48 h-52 bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-2"}>
+
+    </div>
+  );
+};
+
+export default Form;
