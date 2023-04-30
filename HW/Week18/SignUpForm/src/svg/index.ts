a87b4031d7dac8b0d09f3c11711656831a31783b
farzadafi <farzadafi40@gmail.com>
2023-04-30 07:30:15 +0330

    add export for all svg

--- HW/Week18/SignUpForm/src/svg/index.ts
+++ HW/Week18/SignUpForm/src/svg/index.ts
@@ -1,1 +1,4 @@
 export { default as EmailSvg } from './email'
+export { default as CheckSvg } from './check'
+export { default as LockSvg } from './lock'
+export { default as PeopleSvg } from './people'
