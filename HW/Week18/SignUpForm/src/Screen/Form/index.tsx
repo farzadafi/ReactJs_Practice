abba7c710de94b0ed507905e34e40cb9525d4c95
farzadafi <farzadafi40@gmail.com>
2023-04-30 08:21:46 +0330

    fix email input onchange

--- HW/Week18/SignUpForm/src/Screen/Form/index.tsx
+++ HW/Week18/SignUpForm/src/Screen/Form/index.tsx
@@ -1,5 +1,6 @@
 import {Button, Input} from "../../Component";
 import {CheckSvg, EmailSvg, LockSvg, PeopleSvg} from "../../svg";
+import {useState} from "react";
 
 const Email = <EmailSvg/>
 const People = <PeopleSvg/>
@@ -7,6 +8,17 @@ const password = <LockSvg/>
 const confirmPassword = <CheckSvg/>
 
 const Form = () => {
+  const [emailValue, setEmailValue] = useState('')
+
+  const handleOnClick = () => {
+    alert("farzad")
+    alert(emailValue)
+  }
+
+  const onChangeHandler = (value:string) => {
+    setEmailValue(value)
+  }
+
   return (
     <div className={"w-48 h-64 bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-4"}>
       <div className={"flex flex-col gap-4"}>
@@ -14,11 +26,11 @@ const Form = () => {
           <Button title={"Sign In"} classes={""}/>
           <Button title={"Sign Up"} classes={"bg-purple-600 font-white"}/>
         </div>
-        <Input placeHolder={"Email"} svg={Email} name={"email"} type={"email"}/>
+        <Input placeHolder={"Email"} svg={Email} name={"email"} type={"email"} value={emailValue} onChange={(value) => onChangeHandler(value)}/>
         <Input placeHolder={"Username"} svg={People} name={"username"} type={"text"}/>
         <Input placeHolder={"password"} svg={password} name={"password"} type={"password"}/>
         <Input placeHolder={"confirm password"} svg={confirmPassword} name={"confirmPassword"} type={"password"}/>
-        <Button title={"Sign Up"} classes={"bg-purple-600 text-white"}/>
+        <Button title={"Sign Up"} classes={"bg-purple-600 text-white"} onClick={handleOnClick}/>
       </div>
     </div>
   );
