4b9598c213a4a71972217a564af0b325c18915e5
farzadafi <farzadafi40@gmail.com>
2023-04-30 07:28:35 +0330

    fix input component props

--- HW/Week18/SignUpForm/src/Screen/Form/index.tsx
+++ HW/Week18/SignUpForm/src/Screen/Form/index.tsx
@@ -1,13 +1,17 @@
-import {Button} from "../../Component";
+import {Button, Input} from "../../Component";
+import {EmailSvg} from "../../svg";
+
+const Email = <EmailSvg/>
 
 const Form = () => {
   return (
-    <div className={"w-48 h-52 bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-2"}>
-      <div>
-        <div className={"flex gap-2 px-8"}>
+    <div className={"w-48 h-52 bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-4"}>
+      <div className={"flex flex-col gap-2"}>
+        <div className={"flex gap-2 px-6"}>
           <Button title={"Sign In"} classes={""}/>
           <Button title={"Sign Up"} classes={"bg-purple-600 font-white"}/>
         </div>
+        <Input placeHolder={"Email"} svg={Email} name={"email"} type={"email"}/>
       </div>
     </div>
   );
