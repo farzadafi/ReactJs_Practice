321c1c9605e6c41c8f661542fd8ffd8211f4d0ec
farzadafi <farzadafi40@gmail.com>
2023-04-30 07:28:01 +0330

    fix input class name

--- HW/Week18/SignUpForm/src/Component/input/index.tsx
+++ HW/Week18/SignUpForm/src/Component/input/index.tsx
@@ -1,9 +1,11 @@
 interface Props {
   placeHolder: string
   svg: JSX.Element
+  name: string
+  type: string
 }
 
-const Input = ({placeHolder, svg}:Props) => {
+const Input = ({placeHolder, svg, name, type}:Props) => {
   return (
     <div>
       <label htmlFor="email" className="block text-sm font-medium text-gray-700"></label>
@@ -11,8 +13,8 @@ const Input = ({placeHolder, svg}:Props) => {
         <div className="absolute inset-y-0 left-0 pl-1 flex items-center pointer-events-none">
           {svg}
         </div>
-        <input type="email" name="email" id="email"
-               className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-8 sm:text-sm border-gray-300 rounded-sm"
+        <input type={type} name={name}
+               className="text-xs block w-full pl-5 border-gray-300 rounded-sm h-5 flex items-center justify-center"
                placeholder={placeHolder} />
       </div>
     </div>
