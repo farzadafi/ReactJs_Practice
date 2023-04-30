ce86fea5ec0a634c84a0654bb6f8aa45559308f0
farzadafi <farzadafi40@gmail.com>
2023-04-30 05:53:56 +0330

    add two button for sign in and sign up

--- HW/Week18/SignUpForm/src/Screen/Form/index.tsx
+++ HW/Week18/SignUpForm/src/Screen/Form/index.tsx
@@ -1,7 +1,14 @@
+import {Button} from "../../Component";
+
 const Form = () => {
   return (
     <div className={"w-48 h-52 bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-2"}>
-
+      <div>
+        <div className={"flex gap-2 px-8"}>
+          <Button title={"Sign In"} classes={""}/>
+          <Button title={"Sign Up"} classes={"bg-purple-600 font-white"}/>
+        </div>
+      </div>
     </div>
   );
 };
