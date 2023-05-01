91e02bf89b4ccf3cb115c569342be15345b17046
farzadafi <farzadafi40@gmail.com>
2023-05-01 05:18:35 +0330

    add onclick attribute to button

--- HW/Week18/SignUpForm/src/Component/button/index.tsx
+++ HW/Week18/SignUpForm/src/Component/button/index.tsx
@@ -1,14 +1,15 @@
 interface Props {
   title: string
   classes: string
+  onClick?: () => void
 }
 
-const Button = ({title, classes}:Props) => {
+const Button = ({title, classes, onClick}:Props) => {
   return (
     <button type="button"
             className={`p-1 border-transparent text-xs w-full
              rounded-sm shadow-sm hover:bg-purple-900 focus:outline-none
-              focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 ${classes}`}>{title}
+              focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 ${classes}`} onClick={onClick}>{title}
       </button>
 
   );
