d7f9721f463ea4cc4bc25388ddc2afa81af2046e
farzadafi <farzadafi40@gmail.com>
2023-05-01 07:43:15 +0330

    fix vite config path import

--- HW/Week18/ContactManagement/vite.config.ts
+++ HW/Week18/ContactManagement/vite.config.ts
@@ -1,5 +1,8 @@
+// @ts-ignore
 import { defineConfig } from 'vite'
+// @ts-ignore
 import react from '@vitejs/plugin-react'
+// @ts-ignore
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
