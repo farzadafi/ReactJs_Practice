71d651c4f367c7493a0469102a33d2eab1ed4c39
farzadafi <farzadafi40@gmail.com>
2023-05-01 06:21:25 +0330

    fix classes for main div

--- HW/Week18/SignUpForm/src/Screen/Form/index.tsx
+++ HW/Week18/SignUpForm/src/Screen/Form/index.tsx
@@ -44,8 +44,8 @@ const Form = () => {
 
   return (
     <div
-      className={"w-48 h-64 bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-4"}>
-      <div className={"flex flex-col"}>
+      className={"w-48 min-h-64 overflow-y-auto bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-4"}>
+      <div className={"flex flex-col gap-4"}>
         <div className={"flex gap-2 px-6"}>
           <Button title={"Sign In"} classes={""}/>
           <Button title={"Sign Up"} classes={"bg-purple-600 font-white"}/>
