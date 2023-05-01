fe0cb8e019c70d711a0ee0f43b8aecdaf3f2288c
farzadafi <farzadafi40@gmail.com>
2023-05-01 06:13:48 +0330

    add some p tag and state for show mistake to user

--- HW/Week18/SignUpForm/src/Screen/Form/index.tsx
+++ HW/Week18/SignUpForm/src/Screen/Form/index.tsx
@@ -13,11 +13,17 @@ const Form = () => {
   const [passwordValue, setPasswordValue] = useState("");
   const [confirmPasswordValue, setConfirmPasswordValue] = useState("");
 
+  const [isEmailHidden, setIsEmailHidden] = useState(true);
+  const [isUsernameHidden, setIsUsernameHidden] = useState(true);
+  const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+  const [isConfirmPasswordHidden, setIsConfirmPasswordHidden] = useState(true);
+
   const handleOnClick = () => {
-    alert("email value is" + emailValue);
-    alert("username value is" + usernameValue);
-    alert("password value is" + passwordValue);
-    alert("confirm password value is" + confirmPasswordValue);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    emailValue.match(emailRegex) ? setIsEmailHidden(true) : setIsEmailHidden(false);
+    (usernameValue.length > 2) ? setIsUsernameHidden(true) : setIsUsernameHidden(false);
+    (passwordValue.length > 7) ? setIsPasswordHidden(true) : setIsPasswordHidden(false);
+    (confirmPasswordValue.match(passwordValue) ? setIsConfirmPasswordHidden(true) : setIsConfirmPasswordHidden(false))
   };
 
   const onChangeHandler = (value: string) => {
@@ -39,19 +45,43 @@ const Form = () => {
   return (
     <div
       className={"w-48 h-64 bg-[#fdecf6] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-4"}>
-      <div className={"flex flex-col gap-4"}>
+      <div className={"flex flex-col"}>
         <div className={"flex gap-2 px-6"}>
           <Button title={"Sign In"} classes={""}/>
           <Button title={"Sign Up"} classes={"bg-purple-600 font-white"}/>
         </div>
-        <Input placeHolder={"Email"} svg={Email} name={"email"} type={"email"} value={emailValue}
-               onChange={(value) => onChangeHandler(value)}/>
+        <div>
+          <Input placeHolder={"Email"} svg={Email} name={"email"} type={"email"} value={emailValue}
+                 onChange={(value) => onChangeHandler(value)}/>
+          {isEmailHidden ? null : (
+            <p className="text-xs text-red-600 font-thin">
+              please enter a valid Email </p>
+          )}
+        </div>
+        <div>
         <Input placeHolder={"Username"} svg={People} name={"username"} type={"text"}
                onChange={(value) => onChangeUsernameHandler(value)}/>
+          {isUsernameHidden ? null : (
+            <p className="text-xs text-red-600 font-thin">
+              must be more than 3 char </p>
+          )}
+        </div>
+          <div>
         <Input placeHolder={"password"} svg={password} name={"password"} type={"password"}
                onChange={(value) => onChangePasswordHandler(value)}/>
+            {isPasswordHidden ? null : (
+              <p className="text-xs text-red-600 font-thin">
+                more than 8 char </p>
+            )}
+          </div>
+            <div>
         <Input placeHolder={"confirm password"} svg={confirmPassword} name={"confirmPassword"} type={"password"}
                onChange={(value => onChangeConfirmPasswordHandler(value))}/>
+              {isConfirmPasswordHidden ? null : (
+                <p className="text-xs text-red-600 font-thin">
+                  password and confirm have to same </p>
+              )}
+            </div>
         <Button title={"Sign Up"} classes={"bg-purple-600 text-white"} onClick={handleOnClick}/>
       </div>
     </div>
