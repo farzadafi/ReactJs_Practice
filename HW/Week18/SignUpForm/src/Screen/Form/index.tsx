a5b2a95dc878a7450f9db86a913be9a530c4106a
farzadafi <farzadafi40@gmail.com>
2023-05-01 05:24:22 +0330

    add one state and one function for username handler

--- HW/Week18/SignUpForm/src/Screen/Form/index.tsx
+++ HW/Week18/SignUpForm/src/Screen/Form/index.tsx
@@ -9,14 +9,19 @@ const confirmPassword = <CheckSvg/>
 
 const Form = () => {
   const [emailValue, setEmailValue] = useState('')
+  const [usernameValue, setUsernameValue] = useState('')
 
   const handleOnClick = () => {
-    alert("farzad")
-    alert(emailValue)
+    alert("email value is" + emailValue)
+    alert("username value is" + usernameValue)
   }
 
   const onChangeHandler = (value:string) => {
     setEmailValue(value)
+  }
+
+  const onChangeUsernameHandler = (value:string) => {
+    setUsernameValue(value)
   }
 
   return (
@@ -27,7 +32,7 @@ const Form = () => {
           <Button title={"Sign Up"} classes={"bg-purple-600 font-white"}/>
         </div>
         <Input placeHolder={"Email"} svg={Email} name={"email"} type={"email"} value={emailValue} onChange={(value) => onChangeHandler(value)}/>
-        <Input placeHolder={"Username"} svg={People} name={"username"} type={"text"}/>
+        <Input placeHolder={"Username"} svg={People} name={"username"} type={"text"} onChange={(value) => onChangeUsernameHandler(value)}/>
         <Input placeHolder={"password"} svg={password} name={"password"} type={"password"}/>
         <Input placeHolder={"confirm password"} svg={confirmPassword} name={"confirmPassword"} type={"password"}/>
         <Button title={"Sign Up"} classes={"bg-purple-600 text-white"} onClick={handleOnClick}/>
