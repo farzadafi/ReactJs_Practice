96baacc72cc89e01522a4b508b8c5aea61b643c9
farzadafi <farzadafi40@gmail.com>
2023-05-02 08:46:47 +0330

    fix onChange Input

--- HW/Week18/ContactManagement/src/Component/Input/index.tsx
+++ HW/Week18/ContactManagement/src/Component/Input/index.tsx
@@ -1,13 +1,15 @@
 interface Props {
   name: string;
   placeHolder: string;
+  onChange: (value:string) => void
 }
 
-const Input = ({name, placeHolder}: Props) => {
+const Input = ({name, placeHolder, onChange}: Props) => {
   return (
     <div className={"w-full"}>
       <input type="text" name={name}
              className="shadow-sm focus:ring-indigo-500 w-full focus:border-indigo-500 sm:text-sm border border-gray-400 p-1 rounded-md"
+             onChange={e => onChange ? onChange(e.target.value) : "null" }
              placeholder={placeHolder}/>
     </div>
   );
