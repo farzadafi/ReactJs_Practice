a390618a9f78b6cd47bd8ad74ff699e2f40a83bd
farzadafi <farzadafi40@gmail.com>
2023-05-02 08:50:41 +0330

    fix lastname input and add lastnameOnChangeHandler function

--- HW/Week18/ContactManagement/src/Component/Form/index.tsx
+++ HW/Week18/ContactManagement/src/Component/Form/index.tsx
@@ -3,21 +3,27 @@ import {Button, CustomSelect} from "@/Component";
 import {useState} from "react";
 
 const Form = () => {
-  const [firstname, setFirstname] = useState('')
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
 
-  const firstnameOnchangeHandler = (e:string) => {
-    setFirstname(e)
-  }
+  const firstnameOnchangeHandler = (e: string) => {
+    setFirstname(e);
+  };
+
+  const lastnameOnchangeHandler = (e: string) => {
+    setLastname(e);
+  };
 
   const onClickHandler = () => {
-    alert("naser")
-  }
+  };
 
   return (
     <div className={"flex justify-center flex-col items-center gap-4 bg-pink-300 rounded-md mt-2 w-72 p-4 mx-auto"}>
       <p className={"text-center"}>وب اپلیکیشن مدیریت مخاطبین</p>
-      <Input placeHolder={"نام ..."} name={"firstname"} onChange={ (e) => firstnameOnchangeHandler(e)} value={firstname}/>
-      <Input placeHolder={"نام خانوادگی ..."} name={"firstname"} onChange={}/>
+      <Input placeHolder={"نام ..."} name={"firstname"} onChange={(e) => firstnameOnchangeHandler(e)}
+             value={firstname}/>
+      <Input placeHolder={"نام خانوادگی ..."} name={"firstname"} onChange={(e) => lastnameOnchangeHandler(e)}
+             value={lastname}/>
       <Input placeHolder={"شماره تماس ..."} name={"firstname"} onChange={}/>
       <CustomSelect/>
       <Input placeHolder={"ایمیل ..."} name={"firstname"}/>
