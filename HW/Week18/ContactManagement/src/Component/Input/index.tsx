d6a9f245fb28d369489cfabab990a791678c4f6c
farzadafi <farzadafi40@gmail.com>
2023-05-02 08:58:36 +0330

    add value attribute

--- HW/Week18/ContactManagement/src/Component/Input/index.tsx
+++ HW/Week18/ContactManagement/src/Component/Input/index.tsx
@@ -2,14 +2,16 @@ interface Props {
   name: string;
   placeHolder: string;
   onChange: (value:string) => void
+  value: string
 }
 
-const Input = ({name, placeHolder, onChange}: Props) => {
+const Input = ({name, placeHolder, onChange, value}: Props) => {
   return (
     <div className={"w-full"}>
       <input type="text" name={name}
              className="shadow-sm focus:ring-indigo-500 w-full focus:border-indigo-500 sm:text-sm border border-gray-400 p-1 rounded-md"
              onChange={e => onChange ? onChange(e.target.value) : "null" }
+             value={value}
              placeholder={placeHolder}/>
     </div>
   );
