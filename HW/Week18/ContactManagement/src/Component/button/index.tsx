4417457598392c3decf4c41a6b4a9258fb60fd75
farzadafi <farzadafi40@gmail.com>
2023-05-02 08:28:29 +0330

    add onClick handler to button

--- HW/Week18/ContactManagement/src/Component/button/index.tsx
+++ HW/Week18/ContactManagement/src/Component/button/index.tsx
@@ -1,10 +1,12 @@
 interface Props {
   text: string;
+  onClick: () => void;
 }
 
-const Button = ({text}: Props) => {
+const Button = ({text, onClick}: Props) => {
   return (
     <button type="button"
+            onClick={onClick}
             className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
       {text}</button>
   );
