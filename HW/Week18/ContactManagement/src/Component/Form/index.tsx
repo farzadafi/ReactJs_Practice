dd4ba793bddf36eb0d850ce1cb03253b4aa0c32b
farzadafi <farzadafi40@gmail.com>
2023-05-02 08:19:41 +0330

    add new button and customSelect component

--- HW/Week18/ContactManagement/src/Component/Form/index.tsx
+++ HW/Week18/ContactManagement/src/Component/Form/index.tsx
@@ -1,10 +1,16 @@
 import Input from "@/Component/Input";
+import {Button, CustomSelect} from "@/Component";
 
 const Form = () => {
   return (
-    <div className={"flex justify-center flex-col items-center"}>
+    <div className={"flex justify-center flex-col items-center gap-4 bg-pink-300 rounded-md mt-2 w-72 p-4 mx-auto"}>
       <p className={"text-center"}>وب اپلیکیشن مدیریت مخاطبین</p>
       <Input placeHolder={"نام ..."} name={"firstname"}/>
+      <Input placeHolder={"نام خانوادگی ..."} name={"firstname"}/>
+      <Input placeHolder={"شماره تماس ..."} name={"firstname"}/>
+      <CustomSelect/>
+      <Input placeHolder={"ایمیل ..."} name={"firstname"}/>
+      <Button text={"اضافه کردن"}/>
     </div>
   );
 };
