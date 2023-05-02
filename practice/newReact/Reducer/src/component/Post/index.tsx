2960d949e6f59eccbce3705c19c2a575ed3afc10
farzadafi <farzadafi40@gmail.com>
2023-05-02 13:59:14 +0330

    define a useAction function

--- practice/newReact/Reducer/src/component/Post/index.tsx
+++ practice/newReact/Reducer/src/component/Post/index.tsx
@@ -7,37 +7,56 @@ function Post() {
   const [title, setTitle] = useState("");
   const [postId, setPostId] = useState(1);
   const [loading, setLoading] = useState(true);
-  const [customToast, setCustomToast] = useState({type : 'success', message : ''})
+  const [customToast, setCustomToast] = useState({type: "success", message: ""});
+
+  interface Payload {
+    id: number;
+    title: string;
+  }
+
+  function userAction(type: string, payload: Payload) {
+    switch (type) {
+    case "get-post-request" :
+      setPostId(payload.id);
+      setLoading(true);
+      break;
+    case "get-post-success" :
+      setTitle(payload.title);
+      setLoading(false);
+      toast.success(`${payload.id} is fetched :)`);
+    }
+
+  }
 
   useEffect(() => {
     axios(`http://localhost:3000/posts/${postId}`)
       .then(({data}) => {
-        setTitle(data.title)
-        setLoading(false)
-        toast.success(`${postId} is fetched :)`)
+        userAction("get-post-success", {
+          id: data.id,
+          title: data.title
+        })
       });
   }, [postId]);
 
   // @ts-ignore
   const handleChangePost = ({target}) => {
-    setPostId(target.value)
-    setLoading(true)
-  }
+    userAction("get-post-request", {
+      id: target.value,
+      title: "null"
+    });
+  };
 
   return (
     <>
       <div className={"flex justify-center mt-48 flex-col items-center"}>
         <div>
           <label htmlFor="">Post Id :</label>
-          <input
-            type="number"
-            value={postId}
-            className="shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-            onChange={handleChangePost}
-          />
+          <input type="number" value={postId}
+                 className="shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                 onChange={handleChangePost}/>
         </div>
         <div className={"text-blue-500 mt-10"}>
-          { loading ? <span>Loading</span> : <span>{title}</span>}
+          {loading ? <span>Loading</span> : <span>{title}</span>}
         </div>
         <CustomToast type={customToast.type} message={customToast.message}/>
       </div>
