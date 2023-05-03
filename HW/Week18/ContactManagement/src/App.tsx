3c4f3d61d2ba177342e9af62056ee43cb8f7fdf8
farzadafi <farzadafi40@gmail.com>
2023-05-03 08:26:12 +0330

    move all state from Form to App

--- HW/Week18/ContactManagement/src/App.tsx
+++ HW/Week18/ContactManagement/src/App.tsx
@@ -1,12 +1,58 @@
-import './index.css'
+import "./index.css";
 import {Form} from "@/Component";
+import {useState} from "react";
 
 function App() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [relation, setRelation] = useState("");
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const firstnameOnchangeHandler = (e: string) => {
+    setFirstname(e);
+    enableButton();
+  };
+
+  const lastnameOnchangeHandler = (e: string) => {
+    setLastname(e);
+    enableButton();
+  };
+
+  const phoneNumberOnchangeHandler = (e: string) => {
+    setPhoneNumber(e);
+    enableButton();
+  };
+
+  const emailOnchangeHandler = (e: string) => {
+    setEmail(e);
+    enableButton();
+  };
+
+  const onClickHandlerCustomSelect = (e: string) => {
+    setRelation(e);
+    enableButton();
+  };
+
+  function enableButton() {
+    if ((firstname.length > 0) && (lastname.length > 0) && (phoneNumber.length > 6) && (relation.length > 0) && (email.length > 0))
+      setIsDisabled(false);
+  }
+
+  const handleAddCard = () => {
+    alert("firstname");
+  };
+
   return (
     <>
-      <Form/>
+      <Form firstname={firstname} lastname={lastname} email={email} phoneNumber={phoneNumber}
+            firstnameInputChange={firstnameOnchangeHandler} lastnameInputChange={lastnameOnchangeHandler}
+            onAddCard={handleAddCard} phoneNumberInputChange={phoneNumberOnchangeHandler}
+            emailInputChange={emailOnchangeHandler} isDisabled={isDisabled}
+            customSelectInputChange={onClickHandlerCustomSelect} relation={relation} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- HW/Week18/ContactManagement/src/Component/Form/index.tsx
+++ HW/Week18/ContactManagement/src/Component/Form/index.tsx
@@ -2,49 +2,44 @@ import Input from "@/Component/Input";
 import {Button, CustomSelect} from "@/Component";
 import {useState} from "react";
 
-const Form = () => {
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [relation, setRelation] = useState("");
+interface Props {
+  firstname: string;
+  lastname: string;
+  phoneNumber: string;
+  email: string;
+  relation: string;
+  firstnameInputChange: (value:string) => void;
+  lastnameInputChange: (value:string) => void;
+  phoneNumberInputChange: (value:string) => void;
+  emailInputChange: (value:string) => void;
+  onAddCard: () => void;
+  customSelectInputChange: (value:string) => void;
+  isDisabled: boolean;
+}
 
+const Form = ({
+  firstname,
+  lastname,
+  phoneNumber,
+  email,
+  relation,
+  firstnameInputChange,
+  lastnameInputChange,
+  phoneNumberInputChange,
+  emailInputChange,
+  onAddCard,
+  customSelectInputChange,
+  isDisabled
+}:Props) => {
   const [isFirstnameHidden, setIsFirstnameHidden] = useState(true);
   const [isLastnameHidden, setIsLastnameHidden] = useState(true);
   const [isPhoneNumberHidden, setIsPhoneNumberHidden] = useState(true);
   const [isRelationHidden, setIsRelationHidden] = useState(true);
   const [isEmailHidden, setIsEmailHidden] = useState(true);
 
-  const [isDisabled, setIsDisabled] = useState(true);
-
-  const firstnameOnchangeHandler = (e: string) => {
-    setFirstname(e);
-    enableButton();
-  };
-
-  const lastnameOnchangeHandler = (e: string) => {
-    setLastname(e);
-    enableButton();
-  };
-
-  const phoneNumberOnchangeHandler = (e: string) => {
-    setPhoneNumber(e);
-    enableButton();
-  };
-
-  const emailOnchangeHandler = (e: string) => {
-    setEmail(e);
-    enableButton();
-  };
-
-  const onClickHandlerCustomSelect = (e: string) => {
-    setRelation(e);
-    enableButton();
-  };
-
-  function enableButton() {
-    if ((firstname.length > 0) && (lastname.length > 0) && (phoneNumber.length > 6) && (relation.length > 0) && (email.length > 0))
-      setIsDisabled(false);
+  function handleClick() {
+    onAddCard();
+    onClickHandler();
   }
 
   const onClickHandler = () => {
@@ -59,15 +54,14 @@ const Form = () => {
     <div className={"flex justify-center flex-col items-center gap-4 bg-pink-300 rounded-md mt-2 w-72 p-4 mx-auto"}>
       <p className={"text-center"}>وب اپلیکیشن مدیریت مخاطبین</p>
       <div className={"w-full"}>
-        <Input placeHolder={"نام ..."} name={"firstname"} onChange={(e) => firstnameOnchangeHandler(e)}
-               value={firstname}/>
+        <Input placeHolder={"نام ..."} name={"firstname"} onChange={(e) => firstnameInputChange(e)} value={firstname}/>
         {isFirstnameHidden ? null : (
           <p className="text-xs text-red-600 font-thin">
             نام نمیتواند کمتر از ۲ کارکتر باشد </p>
         )}
       </div>
       <div className={"w-full"}>
-        <Input placeHolder={"نام خانوادگی ..."} name={"firstname"} onChange={(e) => lastnameOnchangeHandler(e)}
+        <Input placeHolder={"نام خانوادگی ..."} name={"firstname"} onChange={(e) => lastnameInputChange(e)}
                value={lastname}/>
         {isLastnameHidden ? null : (
           <p className="text-xs text-red-600 font-thin">
@@ -75,7 +69,7 @@ const Form = () => {
         )}
       </div>
       <div className={"w-full"}>
-        <Input placeHolder={"شماره تماس ..."} name={"firstname"} onChange={(e) => phoneNumberOnchangeHandler(e)}
+        <Input placeHolder={"شماره تماس ..."} name={"firstname"} onChange={(e) => phoneNumberInputChange(e)}
                value={phoneNumber}/>
         {isPhoneNumberHidden ? null : (
           <p className="text-xs text-red-600 font-thin">
@@ -83,20 +77,20 @@ const Form = () => {
         )}
       </div>
       <div className={"w-full"}>
-        <CustomSelect onClick={(value) => onClickHandlerCustomSelect(value)}/>
+        <CustomSelect onClick={(value) => customSelectInputChange(value)}/>
         {isRelationHidden ? null : (
           <p className="text-xs text-red-600 font-thin">
             یک نسبت انتخاب کنید </p>
         )}
       </div>
       <div className={"w-full"}>
-        <Input placeHolder={"ایمیل ..."} name={"firstname"} onChange={(e) => emailOnchangeHandler(e)} value={email}/>
+        <Input placeHolder={"ایمیل ..."} name={"firstname"} onChange={(e) => emailInputChange(e)} value={email}/>
         {isEmailHidden ? null : (
           <p className="text-xs text-red-600 font-thin">
             یک ایمیل معتبر وارد کنید </p>
         )}
       </div>
-      <Button isDisabled={isDisabled} onClick={onClickHandler} text={"اضافه کردن"}/>
+      <Button isDisabled={isDisabled} onClick={handleClick} text={"اضافه کردن"}/>
     </div>
   );
 };
