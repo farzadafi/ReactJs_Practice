5bd5e2a5c1386840745ec54fb65be470f1ebe3ad
farzadafi <farzadafi40@gmail.com>
2023-05-03 06:25:32 +0330

    add some p tag and fix onClickHandler function

--- HW/Week18/ContactManagement/src/Component/Form/index.tsx
+++ HW/Week18/ContactManagement/src/Component/Form/index.tsx
@@ -9,6 +9,12 @@ const Form = () => {
   const [email, setEmail] = useState("");
   const [relation, setRelation] = useState("");
 
+  const [isFirstnameHidden, setIsFirstnameHidden] = useState(true);
+  const [isLastnameHidden, setIsLastnameHidden] = useState(true);
+  const [isPhoneNumberHidden, setIsPhoneNumberHidden] = useState(true);
+  const [isRelationHidden, setIsRelationHidden] = useState(true);
+  const [isEmailHidden, setIsEmailHidden] = useState(true);
+
   const firstnameOnchangeHandler = (e: string) => {
     setFirstname(e);
   };
@@ -20,29 +26,64 @@ const Form = () => {
   const phoneNumberOnchangeHandler = (e: string) => {
     setPhoneNumber(e);
   };
-  
+
   const emailOnchangeHandler = (e: string) => {
     setEmail(e);
   };
 
-  const onClickHandlerCustomSelect = (e:string) => {
-    setRelation(e)
-  }
+  const onClickHandlerCustomSelect = (e: string) => {
+    setRelation(e);
+  };
 
   const onClickHandler = () => {
+    firstname.length <= 2 ? setIsFirstnameHidden(false) : setIsFirstnameHidden(true);
+    lastname.length <= 2 ? setIsLastnameHidden(false) : setIsLastnameHidden(true);
+    lastname.length <= 6 ? setIsPhoneNumberHidden(false) : setIsPhoneNumberHidden(true);
+    relation === "" ? setIsRelationHidden(false) : setIsRelationHidden(true);
+    email.length <= 6 ? setIsEmailHidden(false) : setIsEmailHidden(true);
   };
 
   return (
     <div className={"flex justify-center flex-col items-center gap-4 bg-pink-300 rounded-md mt-2 w-72 p-4 mx-auto"}>
       <p className={"text-center"}>وب اپلیکیشن مدیریت مخاطبین</p>
-      <Input placeHolder={"نام ..."} name={"firstname"} onChange={(e) => firstnameOnchangeHandler(e)}
-             value={firstname}/>
-      <Input placeHolder={"نام خانوادگی ..."} name={"firstname"} onChange={(e) => lastnameOnchangeHandler(e)}
-             value={lastname}/>
-      <Input placeHolder={"شماره تماس ..."} name={"firstname"} onChange={(e) => phoneNumberOnchangeHandler(e)}
-             value={phoneNumber}/>
-      <CustomSelect onClick={ (value) => onClickHandlerCustomSelect(value)}/>
-      <Input placeHolder={"ایمیل ..."} name={"firstname"} onChange={ (e) => emailOnchangeHandler(e)} value={email}/>
+      <div className={"w-full"}>
+        <Input placeHolder={"نام ..."} name={"firstname"} onChange={(e) => firstnameOnchangeHandler(e)}
+               value={firstname}/>
+        {isFirstnameHidden ? null : (
+          <p className="text-xs text-red-600 font-thin">
+            نام نمیتواند کمتر از ۲ کارکتر باشد </p>
+        )}
+      </div>
+      <div className={"w-full"}>
+        <Input placeHolder={"نام خانوادگی ..."} name={"firstname"} onChange={(e) => lastnameOnchangeHandler(e)}
+               value={lastname}/>
+        {isLastnameHidden ? null : (
+          <p className="text-xs text-red-600 font-thin">
+            نام خانوادگی نمیتواند کمتر از ۲ کارکتر باشد </p>
+        )}
+      </div>
+      <div className={"w-full"}>
+        <Input placeHolder={"شماره تماس ..."} name={"firstname"} onChange={(e) => phoneNumberOnchangeHandler(e)}
+               value={phoneNumber}/>
+        {isPhoneNumberHidden ? null : (
+          <p className="text-xs text-red-600 font-thin">
+            یک شماره معتبر وارد کنید</p>
+        )}
+      </div>
+      <div className={"w-full"}>
+        <CustomSelect onClick={(value) => onClickHandlerCustomSelect(value)}/>
+        {isRelationHidden ? null : (
+          <p className="text-xs text-red-600 font-thin">
+            یک نسبت انتخاب کنید </p>
+        )}
+      </div>
+      <div className={"w-full"}>
+        <Input placeHolder={"ایمیل ..."} name={"firstname"} onChange={(e) => emailOnchangeHandler(e)} value={email}/>
+        {isEmailHidden ? null : (
+          <p className="text-xs text-red-600 font-thin">
+            یک ایمیل معتبر وارد کنید </p>
+        )}
+      </div>
       <Button onClick={onClickHandler} text={"اضافه کردن"}/>
     </div>
   );
