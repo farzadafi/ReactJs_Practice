c25f6fe6c4febbcfec2ecfabc391da90d09f5ddb
farzadafi <farzadafi40@gmail.com>
2023-05-04 05:19:40 +0330

    move all login to Form

--- HW/Week18/ContactManagement/src/App.tsx
+++ HW/Week18/ContactManagement/src/App.tsx
@@ -1,56 +1,19 @@
 import "./index.css";
 import {Form} from "@/Component";
 import {useState} from "react";
+import {User} from "@/interfaces/userInterface";
 
 function App() {
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [relation, setRelation] = useState("");
-  const [isDisabled, setIsDisabled] = useState(true);
 
-  const firstnameOnchangeHandler = (e: string) => {
-    setFirstname(e);
-    enableButton();
-  };
+  const [user, setUser] = useState<User>();
 
-  const lastnameOnchangeHandler = (e: string) => {
-    setLastname(e);
-    enableButton();
-  };
-
-  const phoneNumberOnchangeHandler = (e: string) => {
-    setPhoneNumber(e);
-    enableButton();
-  };
-
-  const emailOnchangeHandler = (e: string) => {
-    setEmail(e);
-    enableButton();
-  };
-
-  const onClickHandlerCustomSelect = (e: string) => {
-    setRelation(e);
-    enableButton();
-  };
-
-  function enableButton() {
-    if ((firstname.length > 0) && (lastname.length > 0) && (phoneNumber.length > 6) && (relation.length > 0) && (email.length > 0))
-      setIsDisabled(false);
-  }
-
-  const handleAddCard = () => {
-    alert("firstname");
+  const setUserHandler = (user: User) => {
+    setUser(user);
   };
 
   return (
     <>
-      <Form firstname={firstname} lastname={lastname} email={email} phoneNumber={phoneNumber}
-            firstnameInputChange={firstnameOnchangeHandler} lastnameInputChange={lastnameOnchangeHandler}
-            onAddCard={handleAddCard} phoneNumberInputChange={phoneNumberOnchangeHandler}
-            emailInputChange={emailOnchangeHandler} isDisabled={isDisabled}
-            customSelectInputChange={onClickHandlerCustomSelect} relation={relation} />
+      <Form user={user} setUser={setUserHandler}/>
     </>
   );
 }
