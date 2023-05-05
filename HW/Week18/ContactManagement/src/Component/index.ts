a3d4cdb0493b965e4b0a4d72f2d38fcfaa4cba55
farzadafi <farzadafi40@gmail.com>
2023-05-05 19:59:51 +0330

    export * from CardSection

--- HW/Week18/ContactManagement/src/Component/index.ts
+++ HW/Week18/ContactManagement/src/Component/index.ts
@@ -1,3 +1,4 @@
 export { default as Form } from './Form'
 export { default as CustomSelect } from './CustomSelect'
 export { default as Button } from './button'
+export { default as CardSection } from './CardSection'
