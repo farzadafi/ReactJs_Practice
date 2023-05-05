5eafae407423df458829c4e15ed9fdac3f15299a
farzadafi <farzadafi40@gmail.com>
2023-05-05 08:01:39 +0330

    fix useState of user value

--- HW/Week18/ContactManagement/src/Component/Form/index.tsx
+++ HW/Week18/ContactManagement/src/Component/Form/index.tsx
@@ -1,20 +1,22 @@
 import Input from "@/Component/Input";
 import {Button, CustomSelect} from "@/Component";
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
 import {User} from "@/interfaces/userInterface";
 
 interface Props {
-  user?: User;
   setUser: (user: User) => void;
 }
 
-const Form = ({user, setUser}: Props) => {
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [relation, setRelation] = useState("");
+const Form = ({setUser}: Props) => {
   const [isDisabled, setIsDisabled] = useState(true);
+
+  const [userValue, setUserValue] = useState<User>({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phoneNumber: "",
+    relation: ""
+  });
 
   const [isFirstnameHidden, setIsFirstnameHidden] = useState(true);
   const [isLastnameHidden, setIsLastnameHidden] = useState(true);
@@ -23,80 +25,65 @@ const Form = ({user, setUser}: Props) => {
   const [isEmailHidden, setIsEmailHidden] = useState(true);
 
   function enableButton() {
-    if ((firstname.length > 0) && (lastname.length > 0) && (phoneNumber.length > 6) && (relation.length > 0) && (email.length > 0))
+    if ((userValue !== undefined) && (userValue.firstname.length > 0) && (userValue.lastname.length > 0) && (userValue.phoneNumber.length > 1) && (userValue.relation.length > 0) && (userValue.email.length > 0))
       setIsDisabled(false);
   }
 
   function handleClick() {
     if (onClickHandler()) {
       const newUser = {
-        firstname,
-        lastname,
-        email,
-        phoneNumber: +phoneNumber,
-        relation
+        firstname : userValue.firstname,
+        lastname : userValue.lastname,
+        email : userValue.email,
+        phoneNumber: userValue.phoneNumber,
+        relation : userValue.relation
       };
       setUser(newUser);
     }
   }
 
-  const firstnameOnchangeHandler = (e: string) => {
-    setFirstname(e);
-    enableButton();
-  };
-
-  const lastnameOnchangeHandler = (e: string) => {
-    setLastname(e);
-    enableButton();
-  };
-
-
-  const phoneNumberOnchangeHandler = (e: string) => {
-    setPhoneNumber(e);
-    enableButton();
-  };
-
-  const emailOnchangeHandler = (e: string) => {
-    setEmail(e);
+  const onchangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    setUserValue({...userValue, [name]: value});
     enableButton();
   };
 
   const onClickHandlerCustomSelect = (e: string) => {
-    setRelation(e);
+    setUserValue({...userValue, ["relation"]: e});
     enableButton();
   };
 
   const onClickHandler = () => {
-    firstname.length <= 2 ? setIsFirstnameHidden(false) : setIsFirstnameHidden(true);
-    lastname.length <= 2 ? setIsLastnameHidden(false) : setIsLastnameHidden(true);
-    phoneNumber.length <= 6 ? setIsPhoneNumberHidden(false) : setIsPhoneNumberHidden(true);
-    relation === "" ? setIsRelationHidden(false) : setIsRelationHidden(true);
-    email.length <= 6 ? setIsEmailHidden(false) : setIsEmailHidden(true);
-    return ((firstname.length > 2) && (lastname.length > 2) && (phoneNumber.length > 6) && (relation !== "") && (email.length > 2));
+    userValue.firstname.length <= 2 ? setIsFirstnameHidden(false) : setIsFirstnameHidden(true);
+    userValue.lastname.length <= 2 ? setIsLastnameHidden(false) : setIsLastnameHidden(true);
+    userValue.phoneNumber.length <= 6 ? setIsPhoneNumberHidden(false) : setIsPhoneNumberHidden(true);
+    userValue.relation === "" ? setIsRelationHidden(false) : setIsRelationHidden(true);
+    userValue.email.length <= 6 ? setIsEmailHidden(false) : setIsEmailHidden(true);
+    return ((userValue.firstname.length > 2) && (userValue.lastname.length > 2) && (userValue.phoneNumber.length > 6) && (userValue.relation !== "") && (userValue.email.length > 2));
   };
 
   return (
     <div className={"flex justify-center flex-col items-center gap-4 bg-pink-300 rounded-md mt-2 w-72 p-4 mx-auto"}>
       <p className={"text-center"}>وب اپلیکیشن مدیریت مخاطبین</p>
       <div className={"w-full"}>
-        <Input placeHolder={"نام ..."} name={"firstname"} onChange={(e) => firstnameOnchangeHandler(e)}
-               value={firstname}/>
+        <Input placeHolder={"نام ..."} name={"firstname"} onChange={(e) => onchangeHandler(e)}
+               value={userValue.firstname}/>
         {isFirstnameHidden ? null : (
           <p className="text-xs text-red-600 font-thin">
             نام نمیتواند کمتر از ۲ کارکتر باشد </p>
         )}
       </div>
       <div className={"w-full"}>
-        <Input placeHolder={"نام خانوادگی ..."} name={"firstname"} onChange={(e) => lastnameOnchangeHandler(e)}
-               value={lastname}/>
+        <Input placeHolder={"نام خانوادگی ..."} name={"lastname"} onChange={(e) => onchangeHandler(e)}
+               value={userValue.lastname}/>
         {isLastnameHidden ? null : (
           <p className="text-xs text-red-600 font-thin">
             نام خانوادگی نمیتواند کمتر از ۲ کارکتر باشد </p>
         )}
       </div>
       <div className={"w-full"}>
-        <Input placeHolder={"شماره تماس ..."} name={"firstname"} onChange={(e) => phoneNumberOnchangeHandler(e)}
-               value={phoneNumber}/>
+        <Input placeHolder={"شماره تماس ..."} name={"phoneNumber"} onChange={(e) => onchangeHandler(e)}
+               value={userValue.phoneNumber}/>
         {isPhoneNumberHidden ? null : (
           <p className="text-xs text-red-600 font-thin">
             یک شماره معتبر وارد کنید</p>
@@ -110,7 +97,7 @@ const Form = ({user, setUser}: Props) => {
         )}
       </div>
       <div className={"w-full"}>
-        <Input placeHolder={"ایمیل ..."} name={"firstname"} onChange={(e) => emailOnchangeHandler(e)} value={email}/>
+        <Input placeHolder={"ایمیل ..."} name={"email"} onChange={(e) => onchangeHandler(e)} value={userValue.email}/>
         {isEmailHidden ? null : (
           <p className="text-xs text-red-600 font-thin">
             یک ایمیل معتبر وارد کنید </p>
