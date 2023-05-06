7d4250fcc9eca5a901605cf0ab218e8528beda0c
farzadafi <farzadafi40@gmail.com>
2023-05-06 14:36:44 +0330

    add two radio button for filter

--- CW/Week19/onlineShop/src/component/sideBar/index.tsx
+++ CW/Week19/onlineShop/src/component/sideBar/index.tsx
@@ -1,7 +1,32 @@
+const notificationMethods = [
+  { id: 'asc', title: 'Ascending' },
+  { id: 'desc', title: 'Descending' }
+]
+
 const Sidebar = () => {
   return (
-    <div className={"w-[20%] bg-pink-600 m-2 h-[50rem]"}>
-      <h1>Filter Products</h1>
+    <div className={"w-[20%] bg-gray-700 m-2 h-[50rem] p-5 text-white"}>
+      <h1 className={"text-2xl "}>Filter Products</h1>
+      <div className={"text-white"}>
+        <fieldset className="mt-4">
+          <div className="space-y-4">
+            {notificationMethods.map((notificationMethod) => (
+              <div key={notificationMethod.id} className="flex items-center">
+                <input
+                  id={notificationMethod.id}
+                  name="notification-method"
+                  type="radio"
+                  defaultChecked={notificationMethod.id === 'email'}
+                  className="focus:ring-indigo-500 h-4 w-4 border-gray-300 hover:cursor-pointer"
+                />
+                <label htmlFor={notificationMethod.id} className="ml-3 block text-sm font-medium hover:cursor-pointer">
+                  {notificationMethod.title}
+                </label>
+              </div>
+            ))}
+          </div>
+        </fieldset>
+      </div>
     </div>
   );
 };
