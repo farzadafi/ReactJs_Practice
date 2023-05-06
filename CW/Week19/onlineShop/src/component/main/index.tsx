957a63f62aadd04aabc1590acd1fbefe1bdebb35
farzadafi <farzadafi40@gmail.com>
2023-05-06 14:27:06 +0330

    fix main width

--- CW/Week19/onlineShop/src/component/main/index.tsx
+++ CW/Week19/onlineShop/src/component/main/index.tsx
@@ -1,18 +1,22 @@
 import {ProductInterface} from "../../interfaces/ProductInterface";
 import Card from "../card";
+import Sidebar from "../sideBar";
 
 interface Props {
-  products : ProductInterface[]
+  products: ProductInterface[];
 }
 
-const Main = ({products}:Props) => {
+const Main = ({products}: Props) => {
   return (
-    <div className={"p-5 flex flex-wrap gap-8"}>
-      {
-        products.length > 0 ? products.map(item => {
-         return <Card product={item}/>
-        }) : null
-      }
+    <div className={"flex w-full bg-pink-[500]"}>
+      <Sidebar/>
+      <div className={"p-5 flex flex-wrap gap-8 w-[100%]"}>
+        {
+          products.length > 0 ? products.map(item => {
+            return <Card product={item}/>;
+          }) : null
+        }
+      </div>
     </div>
   );
 };
