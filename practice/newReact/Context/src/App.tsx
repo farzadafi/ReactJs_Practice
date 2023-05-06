cd8225714f1591e8a0e72fef077ba97d382a490f
farzadafi <farzadafi40@gmail.com>
2023-05-06 08:56:29 +0330

    add all component

--- practice/newReact/Context/src/App.tsx
+++ practice/newReact/Context/src/App.tsx
@@ -1,9 +1,70 @@
-function App() {
+import React, {createContext, useContext, useState} from "react";
 
+function Counter() {
+  const {counter, handleClick} = useCounter()
   return (
-    <>
-    </>
-  )
+    <div>
+      <h1>I'm counter Component</h1>
+      {counter}
+      <button onClick={handleClick}>increment</button>
+    </div>
+  );
 }
 
-export default App
+function Child() {
+  return (
+    <div>
+      <h1>I'm Child Component</h1>
+      <Counter/>
+    </div>
+  );
+}
+
+function Parent() {
+  return (
+    <div>
+      <h1>I'm Parent Component</h1>
+      <Child/>
+    </div>
+  );
+}
+
+type CounterContextType = {
+  counter: number;
+  handleClick: () => void
+}
+
+const CounterContext = createContext<null | CounterContextType>(null);
+const useCounter = () => {
+  return useContext(CounterContext)
+}
+
+const CounterProvider = ({children}: { children: React.ReactNode }) => {
+  const [counter, setCounter] = useState(0);
+
+  function handleClick() {
+    setCounter(prev => prev + 1);
+  }
+
+  return (
+    <CounterContext.Provider value={{counter, handleClick}}>
+      {children}
+    </CounterContext.Provider>
+  );
+};
+
+function App() {
+
+
+  return (
+    <CounterProvider>
+      <div>
+        <h1>I'm Root Component</h1>
+        <Parent/>
+      </div>
+    </CounterProvider>
+  )
+    ;
+}
+
+export default App;
