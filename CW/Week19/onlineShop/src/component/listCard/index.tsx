2a9eafda28c66328934c196695a71273fbcc46fe
farzadafi <farzadafi40@gmail.com>
2023-05-08 15:50:27 +0330

    create ListCard

---
+++ CW/Week19/onlineShop/src/component/listCard/index.tsx
@@ -0,0 +1,25 @@
+import {ProductInterface} from "../../interfaces/ProductInterface";
+import {RiDeleteBin7Fill} from "react-icons/all";
+
+interface Props {
+  product: ProductInterface;
+}
+
+const ListCard = ({product}: Props) => {
+  return (
+    <div className={"flex justify-between items-center hover:cursor-auto"}>
+      <div className={"flex gap-3"}>
+        <img className={"w-14 h-14 rounded-[50%]"} src={product.image} alt="product image"/>
+        <div className={"flex flex-col justify-center items-start"}>
+          <p>{product.name}</p>
+          <p>{product.price}</p>
+        </div>
+      </div>
+      <button>
+        <RiDeleteBin7Fill/>
+      </button>
+    </div>
+  );
+};
+
+export default ListCard;
