5ec36ff7f86bc26b9e90b944df87db99d963e3ab
farzadafi <farzadafi40@gmail.com>
2023-05-09 10:11:07 +0330

    add a key for each ListCard that create into products map

--- CW/Week19/onlineShop/src/layout/header/index.tsx
+++ CW/Week19/onlineShop/src/layout/header/index.tsx
@@ -5,7 +5,7 @@ import {IoCartSharp, MdOutlineArrowDropDown} from "react-icons/all";
 
 const Header = () => {
   const [searchInput, setSearchInput] = useState("");
-  const {products, addToCard} = useShop();
+  const {products} = useShop();
   const [isEnable, setEnable] = useState(true);
   //
   const onchangeHandler = (value: string) => {
@@ -27,8 +27,8 @@ const Header = () => {
         <div
           className={"text-black p-3 flex flex-col gap-3 top-[3.3rem] right-[15.6rem] w-96 rounded-md bg-white border border-slate-500 " + (isEnable ? "hidden" : "absolute")}>
           {
-            products.length > 0 ? products.map(item => {
-              return <ListCard product={item}/>;
+            products.length > 0 ? products.map((item, index) => {
+              return <ListCard key={index} product={item}/>;
             }) : <p>Cart is Empty!</p>
           }
           {
