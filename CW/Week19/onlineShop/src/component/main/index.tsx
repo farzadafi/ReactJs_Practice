9ea17ecdd948df458f5d2e60b33e6d441d31186c
farzadafi <farzadafi40@gmail.com>
2023-05-09 14:51:58 +0330

    add a handle click function

--- CW/Week19/onlineShop/src/component/main/index.tsx
+++ CW/Week19/onlineShop/src/component/main/index.tsx
@@ -1,15 +1,21 @@
 import {ProductInterface} from "../../interfaces/ProductInterface";
 import Card from "../card";
+import * as React from 'react';
 import Sidebar from "../sideBar";
 
 interface Props {
   products: ProductInterface[];
 }
 
+const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
+  const target = e.target as HTMLInputElement;
+  console.log(target.id);
+}
+
 const Main = ({products}: Props) => {
   return (
     <div className={"flex w-full bg-pink-[500]"}>
-      <Sidebar/>
+      <Sidebar onClick={handleClick}/>
       <div className={"p-5 flex flex-wrap gap-8 w-[100%]"}>
         {
           products.length > 0 ? products.map(item => {
