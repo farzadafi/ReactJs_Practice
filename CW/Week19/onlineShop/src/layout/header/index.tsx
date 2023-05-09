f11d34061c7061444e0549adeefcbe004a922162
farzadafi <farzadafi40@gmail.com>
2023-05-09 05:54:39 +0330

    add a new p tag for when cart is empty

--- CW/Week19/onlineShop/src/layout/header/index.tsx
+++ CW/Week19/onlineShop/src/layout/header/index.tsx
@@ -1,20 +1,20 @@
 import {Input, ListCard, useShop} from "../../component";
-import { useState } from "react";
+import {useState} from "react";
 import Button from "../../component/button";
 import {IoCartSharp, MdOutlineArrowDropDown} from "react-icons/all";
 
 const Header = () => {
   const [searchInput, setSearchInput] = useState("");
-  const { products, addToCard} = useShop();
-  const [isEnable, setEnable] = useState(true)
+  const {products, addToCard} = useShop();
+  const [isEnable, setEnable] = useState(true);
   //
   const onchangeHandler = (value: string) => {
     setSearchInput(value);
   };
 
   const onClickHandler = () => {
-    isEnable ? setEnable(false) : setEnable(true)
-  }
+    isEnable ? setEnable(false) : setEnable(true);
+  };
 
   return (
     <header className={"flex bg-gray-700 justify-center items-center gap-40 h-16 text-white"}>
@@ -24,13 +24,17 @@ const Header = () => {
         <IoCartSharp className={"text-4xl"}/>
         <p>{products.length}</p>
         <MdOutlineArrowDropDown className={"text-2xl"}/>
-        <div className={'text-black p-3 flex flex-col gap-3 top-[3.3rem] right-[15.6rem] w-96 rounded-md bg-white border border-slate-500 ' + (isEnable ? 'hidden' : 'absolute')}>
+        <div
+          className={"text-black p-3 flex flex-col gap-3 top-[3.3rem] right-[15.6rem] w-96 rounded-md bg-white border border-slate-500 " + (isEnable ? "hidden" : "absolute")}>
           {
             products.length > 0 ? products.map(item => {
               return <ListCard product={item}/>;
-            }) : null
+            }) : <p>Cart is Empty!</p>
           }
-          <Button variant={"addToCard"} classes={"w-full text-white"} children={"Go To Cart"}/>
+          {
+            products.length > 0 ?
+              <Button variant={"addToCard"} classes={"w-full text-white"} children={"Go To Cart"}/> : null
+          }
         </div>
       </Button>
     </header>
