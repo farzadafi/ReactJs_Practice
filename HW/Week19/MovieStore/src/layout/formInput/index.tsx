20e6e168808c84584d872cb817f90855d2c5fca9
farzadafi <farzadafi40@gmail.com>
2023-05-10 10:08:01 +0330

    fix padding for main div

--- HW/Week19/MovieStore/src/layout/formInput/index.tsx
+++ HW/Week19/MovieStore/src/layout/formInput/index.tsx
@@ -2,7 +2,7 @@ import {Button, Input, TextArea} from "@/component";
 
 const FormInput = () => {
   return (
-    <div className={"bg-gray-700 p-10 text-white flex gap-16"}>
+    <div className={"bg-gray-700 p-14 text-white flex gap-16"}>
       <div className={"space-y-10 w-[25%]"}>
         <Input labelName={"نام فیلم"} placeHolder={"نام فیلم را وارد کنید"}/>
         <Input labelName={"کارگردان"} placeHolder={"نام کارگردان را وارد کنید"}/>
