c78d2e9650645af147a0910ec059d783e733d34a
farzadafi <farzadafi40@gmail.com>
2023-05-10 06:07:52 +0330

    add filteredProduct function for filter product

--- CW/Week19/onlineShop/src/component/main/index.tsx
+++ CW/Week19/onlineShop/src/component/main/index.tsx
@@ -1,24 +1,46 @@
 import {ProductInterface} from "../../interfaces/ProductInterface";
 import Card from "../card";
-import * as React from 'react';
+import * as React from "react";
+import {useState} from "react";
 import Sidebar from "../sideBar";
 
 interface Props {
   products: ProductInterface[];
 }
 
-const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
-  const target = e.target as HTMLInputElement;
-  console.log(target.id);
-}
 
 const Main = ({products}: Props) => {
+  const [filterProduct, setFilterProduct] = useState("");
+
+  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setFilterProduct(target.id);
+  };
+
+  const filteredProduct = (products: ProductInterface[]) => {
+    let resultProduct;
+    switch (filterProduct) {
+    case "":
+      resultProduct = products;
+      break;
+    case "asc" :
+      resultProduct = products.sort((a, b) => parseInt(a.price) - parseInt(b.price));
+      break;
+    case "desc" :
+      resultProduct = products.sort((a, b) => parseInt(b.price) - parseInt(a.price));
+      break;
+    default :
+      resultProduct = products;
+    }
+    return resultProduct;
+  };
+
   return (
     <div className={"flex w-full bg-pink-[500]"}>
       <Sidebar onClick={handleClick}/>
       <div className={"p-5 flex flex-wrap gap-8 w-[100%]"}>
         {
-          products.length > 0 ? products.map(item => {
+          products.length > 0 ? filteredProduct(products).map(item => {
             return <Card id={item.id} key={item.id} product={item}/>;
           }) : null
         }
