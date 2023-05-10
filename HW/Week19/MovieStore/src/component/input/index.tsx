314fb91ed0fde89c16be53fa83f43eb85502e1e5
farzadafi <farzadafi40@gmail.com>
2023-05-10 09:04:59 +0330

    implement body of Input component

--- HW/Week19/MovieStore/src/component/input/index.tsx
+++ HW/Week19/MovieStore/src/component/input/index.tsx
@@ -1,6 +1,23 @@
-const Input = () => {
+interface Props {
+  labelName: string,
+  placeHolder: string,
+
+}
+
+const Input = ({labelName, placeHolder}: Props) => {
   return (
     <div>
+      <div className={"flex gap-2 items-center"}>
+        <span className="w-2 h-5 bg-yellow-300 rounded-sm inline-block"></span>
+        <label className="block text-sm font-medium text-gray-700 text-white text-xl">
+          {labelName}
+        </label>
+      </div>
+      <div className="mt-3">
+        <input
+          className="focus:border-yellow-300 outline-none bg-gray-700 w-full sm:text-sm border-2 border-gray-200 rounded-md p-4"
+          placeholder={placeHolder}/>
+      </div>
     </div>
   );
 };
