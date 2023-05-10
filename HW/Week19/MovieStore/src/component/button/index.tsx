cfeedd9464c19e6f1c3d8953d086179dd09071d3
farzadafi <farzadafi40@gmail.com>
2023-05-10 10:02:11 +0330

    add cancelSave variants

--- HW/Week19/MovieStore/src/component/button/index.tsx
+++ HW/Week19/MovieStore/src/component/button/index.tsx
@@ -2,6 +2,7 @@ import {ReactNode} from "react";
 
 const variants: Record<string, string> = {
   saveFilm: "bg-yellow-500 rounded-md p-3 w-28",
+  cancelSave: "rounded-md p-3 w-28 border border-2 border-gray-500"
 };
 
 type buttonProps = {
