81f3b05112d79c192e7f28ac0c4de44b2763ffa1
farzadafi <farzadafi40@gmail.com>
2023-05-10 09:25:06 +0330

    add instructor input

--- HW/Week19/MovieStore/src/layout/formInput/index.tsx
+++ HW/Week19/MovieStore/src/layout/formInput/index.tsx
@@ -1,6 +1,12 @@
+import {Input} from "@/component";
+
 const FormInput = () => {
   return (
-    <div>
+    <div className={"bg-gray-700 p-10 text-white flex gap-16"}>
+      <div className={"space-y-10 w-[30%]"}>
+        <Input labelName={"نام فیلم"} placeHolder={"نام فیلم را وارد کنید"}/>
+        <Input labelName={"کارگردان"} placeHolder={"نام کارگردان را وارد کنید"}/>
+      </div>
     </div>
   );
 };
