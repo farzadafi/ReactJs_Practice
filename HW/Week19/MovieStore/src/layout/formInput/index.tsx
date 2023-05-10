3dafd5a05702fbdbb977811b121c056216a052b2
farzadafi <farzadafi40@gmail.com>
2023-05-10 09:28:28 +0330

    add two new input for goner and generation date

--- HW/Week19/MovieStore/src/layout/formInput/index.tsx
+++ HW/Week19/MovieStore/src/layout/formInput/index.tsx
@@ -7,6 +7,10 @@ const FormInput = () => {
         <Input labelName={"نام فیلم"} placeHolder={"نام فیلم را وارد کنید"}/>
         <Input labelName={"کارگردان"} placeHolder={"نام کارگردان را وارد کنید"}/>
       </div>
+      <div className={"space-y-10 w-[30%]"}>
+        <Input labelName={"ژانر فیلم"} placeHolder={"نام فیلم را وارد کنید"}/>
+        <Input labelName={"سال تولید"} placeHolder={"سال ساخت فیلم را وارد کنید"}/>
+      </div>
     </div>
   );
 };
