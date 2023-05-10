28f83b7a8c25d6a670257797eb7c353856f5df81
farzadafi <farzadafi40@gmail.com>
2023-05-10 14:27:02 +0330

    export YellowLabel

--- HW/Week19/MovieStore/src/component/index.ts
+++ HW/Week19/MovieStore/src/component/index.ts
@@ -1,3 +1,4 @@
 export { default as Input } from './input'
 export { default as TextArea } from './textArea'
 export { default as Button } from './button'
+export { default as YellowLabel } from './yellowLable'
