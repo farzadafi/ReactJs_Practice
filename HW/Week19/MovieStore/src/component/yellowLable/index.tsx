41d98d4a6dac206945d196ba929f68f9a4b30e70
farzadafi <farzadafi40@gmail.com>
2023-05-10 14:22:15 +0330

    add new classes variable to Props

--- HW/Week19/MovieStore/src/component/yellowLable/index.tsx
+++ HW/Week19/MovieStore/src/component/yellowLable/index.tsx
@@ -1,11 +1,12 @@
 interface Props {
-  labelName : string
+  labelName : string,
+  classes? : string
 }
 
-const YellowLabel = ({labelName}:Props) => {
+const YellowLabel = ({labelName, classes}:Props) => {
   return (
     <>
-      <div className={"flex gap-2 items-center"}>
+      <div className={`flex gap-2 items-center ${classes}`}>
         <span className="w-2 h-5 bg-yellow-300 rounded-sm inline-block"></span>
         <label className="block text-sm font-medium text-gray-700 text-white text-xl">
           {labelName}
