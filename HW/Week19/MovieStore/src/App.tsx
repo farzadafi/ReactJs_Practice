35419b795608c60d798d76c126d8f1de9893ee48
farzadafi <farzadafi40@gmail.com>
2023-05-11 07:12:24 +0330

    move all tag inside MovieProvide

--- HW/Week19/MovieStore/src/App.tsx
+++ HW/Week19/MovieStore/src/App.tsx
@@ -1,18 +1,21 @@
-import './index.css'
+import "./index.css";
 import {Container, FormInput, Header} from "@/layout";
+import {MovieProvider} from "@/context";
 
 function App() {
 
   return (
     <>
       <div>
-        <Container>
-          <Header/>
-          <FormInput/>
-        </Container>
+        <MovieProvider>
+          <Container>
+            <Header/>
+            <FormInput/>
+          </Container>
+        </MovieProvider>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
