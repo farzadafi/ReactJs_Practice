7a66e2ddbc9dd5ccbb997eae2d89dcec06d5290e
farzadafi <farzadafi40@gmail.com>
2023-05-11 07:14:23 +0330

    fix saveFilm an cancelSave button

--- HW/Week19/MovieStore/src/component/button/index.tsx
+++ HW/Week19/MovieStore/src/component/button/index.tsx
@@ -3,8 +3,8 @@ import {ReactNode} from "react";
 const sameClassName = 'rounded-md p-3 w-28 ';
 
 const variants: Record<string, string> = {
-  saveFilm: `${sameClassName} bg-yellow-500`,
-  cancelSave: `${sameClassName} border border-2 border-gray-500`
+  saveFilm: `${sameClassName} bg-yellow-500 hover:bg-yellow-400`,
+  cancelSave: `${sameClassName} border border-2 border-gray-500 hover:bg-red-500`
 };
 
 type buttonProps = {
