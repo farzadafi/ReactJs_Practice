632fa6b95c3ee1a8bfa7a4a79f932e2837f4295e
farzadafi <farzadafi40@gmail.com>
2023-05-12 15:37:37 +0330

    fix onChange state

--- HW/Week19/MovieStore/src/component/CustomSelect/index.tsx
+++ HW/Week19/MovieStore/src/component/CustomSelect/index.tsx
@@ -3,7 +3,7 @@ import {Listbox, Transition} from "@headlessui/react";
 import {CgArrowsExchangeAltV} from "react-icons/all";
 import {YellowLabel} from "@/component";
 
-const people = [
+const goner = [
   {id: 1, name: "اکشن"},
   {id: 2, name: "کمدی"},
   {id: 3, name: "درام"}
@@ -11,7 +11,7 @@ const people = [
 
 interface Props {
   labelName: string;
-  onClick: (e:React.MouseEvent<HTMLSpanElement>) => void;
+  onClick: (e: string) => void;
 }
 
 function classNames(...classes: string[]) {
@@ -19,54 +19,44 @@ function classNames(...classes: string[]) {
 }
 
 const CustomSelect = ({labelName, onClick}: Props) => {
-  const [selected, setSelected] = useState(people[0])
-  console.log(selected);
+  const [selectedValue, setSelected] = useState(goner[0]);
 
   return (
-    <Listbox value={selected} onChange={setSelected}>
-      {({ open }) => (
+    <Listbox value={selectedValue} onChange={setSelected}>
+      {({open}) => (
         <>
           <YellowLabel labelName={labelName} classes={"-mb-7"}/>
           <div className="relative">
             <Listbox.Button className="relative w-full border-2 border-gray-500 rounded-md shadow-sm p-3 ">
-              <span className="block truncate">{selected.name}</span>
+              <span className="block truncate">{selectedValue.name}</span>
               <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
               <CgArrowsExchangeAltV className={"w-8 h-8"}/>
               </span>
             </Listbox.Button>
 
-            <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Listbox.Options className="absolute z-10 mt-1 w-full bg-gray-600 text-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                {people.map((person) => (
-                  <Listbox.Option
-                    key={person.id}
-                    className={({ active }) =>
-                      classNames(
-                        active ? 'text-white bg-yellow-500 hover:cursor-pointer' : 'text-white',
-                        'cursor-default select-none relative py-2 pl-3 pr-9'
-                      )
-                    }
-                    value={person}
-                  >
-                    {({ selected, active }) => (
+            <Transition show={open} as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100"
+                        leaveTo="opacity-0">
+              <Listbox.Options
+                className="absolute z-10 mt-1 w-full bg-gray-600 text-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                {goner.map((item) => (
+                  <Listbox.Option key={item.id} className={({active}) =>
+                    classNames(
+                      active ? "text-white bg-yellow-500 hover:cursor-pointer" : "text-white",
+                      "cursor-default select-none relative py-2 pl-3 pr-9"
+                    )
+                  } value={item}>
+                    {({selected, active}) => (
                       <>
-                        <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                          {person.name}
+                        <span onClick={() => onClick ? onClick(selectedValue.name) : null}
+                              className={classNames(selected ? "font-semibold" : "font-normal", "block truncate")}>
+                          {item.name}
                         </span>
 
                         {selected ? (
-                          <span
-                            className={classNames(
-                              active ? 'text-white' : 'text-indigo-600',
-                              'absolute inset-y-0 right-0 flex items-center pr-4'
-                            )}
-                          >
+                          <span className={classNames(
+                            active ? "text-white" : "text-indigo-600",
+                            "absolute inset-y-0 right-0 flex items-center pr-4"
+                          )}>
                           </span>
                         ) : null}
                       </>
@@ -83,11 +73,3 @@ const CustomSelect = ({labelName, onClick}: Props) => {
 };
 
 export default CustomSelect;
-// onClick={(e) => onClick ? onClick(e) : null}
-// <YellowLabel labelName={labelName} classes={"-mb-7"}/>
-// <CgArrowsExchangeAltV className={"w-8 h-8"}/>
-
-
-// Listbox.option
-// className="absolute z-10 mt-1 w-full bg-gray-600 text-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-
