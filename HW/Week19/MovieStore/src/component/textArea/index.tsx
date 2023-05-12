b9a53754550b98c6ebc7b2bb23257d850bd9363c
farzadafi <farzadafi40@gmail.com>
2023-05-12 15:34:23 +0330

    add name and onChange attribute

--- HW/Week19/MovieStore/src/component/textArea/index.tsx
+++ HW/Week19/MovieStore/src/component/textArea/index.tsx
@@ -1,17 +1,21 @@
 import {YellowLabel} from "@/component";
+import {ChangeEvent} from "react";
 
 interface Props {
   labelName: string,
   placeHolder: string,
+  name: string
+  onChange?: (value: ChangeEvent<HTMLTextAreaElement>) => void
 }
 
-const TextArea = ({labelName, placeHolder}: Props) => {
+const TextArea = ({labelName, placeHolder, onChange, name}: Props) => {
   return (
     <div>
       <YellowLabel labelName={labelName}/>
       <div className="mt-4">
-        <textarea rows={4} name="comment" id="comment"
+        <textarea rows={4} name={name}
                   className="resize-none focus:border-yellow-300 outline-none bg-gray-700 w-full sm:text-sm border-2 border-gray-500 rounded-md p-4"
+                  onChange={e => onChange ? onChange(e) : null}
                   placeholder={placeHolder}/>
       </div>
     </div>
