3e3d946ff636a314044b0a51026c50c4f7423de3
farzadafi <farzadafi40@gmail.com>
2023-05-12 05:31:51 +0330

    add a myType to Props

--- HW/Week19/MovieStore/src/component/input/index.tsx
+++ HW/Week19/MovieStore/src/component/input/index.tsx
@@ -3,17 +3,19 @@ import {YellowLabel} from "@/component";
 interface Props {
   labelName: string,
   placeHolder: string,
-
+  myType?: string
 }
 
-const Input = ({labelName, placeHolder}: Props) => {
+const Input = ({labelName, placeHolder, myType}: Props) => {
   return (
     <div>
       <YellowLabel labelName={labelName}/>
       <div className="mt-3">
         <input
           className="focus:border-yellow-300 outline-none bg-gray-700 w-full sm:text-sm border-2 border-gray-500 rounded-md p-4"
+          type={myType}
           placeholder={placeHolder}/>
+
       </div>
     </div>
   );
