414568aa965f9f63e3c06b0630578e343494b935
farzadafi <farzadafi40@gmail.com>
2023-05-17 09:42:53 +0330

    change import from counterReducer to counterSlice

--- practice/newReact/redux/src/component/counter.tsx
+++ practice/newReact/redux/src/component/counter.tsx
@@ -1,6 +1,5 @@
-import {addByValue, decrement, increment} from "../redux/actions/counter.action";
 import {useDispatch, useSelector} from "react-redux";
-import {counterSelected} from "../redux/reducer/counter.reducer";
+import {addByValue, counterSelected, decrement, increment} from "../redux/slices/counter.slice";
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -11,7 +10,7 @@ const Counter = () => {
       <button onClick={() => dispatch(increment())}>increment</button>
       <span>{counter}</span>
       <button onClick={() => dispatch(decrement())}>decrement</button>
-      <button onClick={ () => dispatch(addByValue(5))}>plus with five</button>
+      <button onClick={ () => dispatch(addByValue({value:5}))}>plus with five</button>
     </div>
   );
 };
