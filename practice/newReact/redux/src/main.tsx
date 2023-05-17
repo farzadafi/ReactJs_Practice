fce88fe0436cbdcd4640e89ec7f06eca3a01d5fa
farzadafi <farzadafi40@gmail.com>
2023-05-17 09:00:47 +0330

    add Provider

--- practice/newReact/redux/src/main.tsx
+++ practice/newReact/redux/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  </Provider>
+);
